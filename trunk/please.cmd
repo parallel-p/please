e27e9959cdf7288933f61367244e81b3c0ef1174
dgozman@google.com <dgozman@google.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2011-01-23 20:14:56 +0000

    Renamed files to default.package and generate.please

--- trunk/please.cmd
+++ trunk/please.cmd
@@ -1,1 +1,2 @@
-python E:\Vitaly\please\please.py %*
+rem Temporary set full path to please.py below.
+python please.py %*
--- trunk/please/config.py
+++ trunk/please/config.py
@@ -3,8 +3,8 @@
 """Configuration."""
 
 PLEASE_WORK_DIR = '.please'
-PLEASE_PROBLEM_FILE = 'problem.properties'
+PLEASE_PROBLEM_FILE = 'default.package'
 
 import os.path
-PLEASE_GENERATE_FILE = os.path.join('tests', 'generate')
+PLEASE_GENERATE_FILE = os.path.join('tests', 'generate.please')
 
