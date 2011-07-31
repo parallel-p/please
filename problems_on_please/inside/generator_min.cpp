0db3f05e299a98a860af3c4b31fee7be2ac331ef
goldvitaly@yandex-team.ru <goldvitaly@yandex-team.ru@cb711904-5dbb-0428-5097-0481b5fa17eb>
2011-07-31 11:37:26 +0000

    add inside example

---
+++ problems_on_please/inside/generator_min.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cassert>
+#include <cstring>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+
+ll rdtsc() {
+    ll tmp;
+    asm("rdtsc" : "=A"(tmp));
+    return tmp;
+}
+
+int main() {
+    srand(rdtsc());
+    const int MAXN = 10;
+    const int MAXLEN = 5;
+
+    for (int t = 0; t < 2; t++) {
+        int n = MAXN - 1;
+        printf("%d\n", n);
+        for (int i = 0; i < n; i++) {
+            int len = MAXLEN;
+            printf("%d", rand() % 9 + 1);
+            for (int j = 0; j < len - 1; j++)
+                printf("%d", rand() % 10);
+            if (i < n - 1)
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
