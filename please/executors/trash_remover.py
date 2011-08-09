42a23aa714e9016e7fed2b789b3c6f1d60da96a8
dubov94 <dubov94@cb711904-5dbb-0428-5097-0481b5fa17eb>
2011-08-09 10:16:24 +0000

    logs in clean up

--- please/executors/trash_remover.py
+++ please/executors/trash_remover.py
@@ -1,13 +1,17 @@
 import os
+import logging
 
 def remove_trash (diff, is_trash):
     for token in diff:
         if is_trash(token):
             os.remove(token)
 
-def remove_logs_in_depth(logs_names_list, base_dir):
+
+def remove_logs_in_depth(logs_names_list, base_dir):            
+    logger = logging.getLogger("please_logger.executors.remove_logs_in_depth")
     for root, dirs, files in os.walk(base_dir):
         for file in files:
             for log in logs_names_list:
                 if file == log:
+                    logger.info("Removing log " + log + " in " + root)
                     os.remove(os.path.join(root, log))
--- please/solution_tester/tester_test.py
+++ please/solution_tester/tester_test.py
@@ -166,7 +166,7 @@ class SolutionTesterTest(unittest.TestCase):
                          ({"WA":[self.a(".tests","1")]}, [], {self.a(".tests","1"):\
                         [self.q("WA"), "stdout", "stderr"]}))
     
-    def test_DifficultExpectedMLUnexpectedTLandOK(self):
+    def test_DifficultExpectedMLUnexpectedTLandOK(self): # pragma: no cover
         config = {}
         config["solution_config"] = None
         config["solution_args"] = None
