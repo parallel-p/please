7383e05ddc8fd3e7b7ea62e7db96b43dda297aad
gurovic@gmail.com <gurovic@users.noreply.github.com>
2011-08-13 17:39:34 +0000

    False instead false )))

--- please/language_configurator/exe.py
+++ please/language_configurator/exe.py
@@ -4,7 +4,7 @@ class ExeConfigurator:
     def get_run_command(self, source):
         return "{0}".format(source)
     def is_compile_garbage (self, source) :
-        return false
+        return False
 
 def get_exe_configurator():
     return ExeConfigurator()
