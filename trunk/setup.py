6d12b33910a6379c86a6d3cd9def3b1a947f094c
A.Max95@gmail.com <A.Max95@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2011-08-13 21:55:35 +0000

    alpha-version of installer

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,64 +1,73 @@
-import glob
-import os
-import shutil
 import sys
-import logging
-from distutils.core import setup
-from please.utils import platform_detector
-     
-def make_dir(dirr):
-     if not(os.path.exists(dirr)):
-          os.mkdir(dirr)
-          logger.info("Make dir: "+dirr)
 
-def copy_file(src,dst):
-     logger.info("Copy file "+src+" in "+dst)
-     shutil.copyfile(src, dst)
+import distribute_setup
+distribute_setup.use_setuptools()
 
-def delete_garbage():
-     logger.info("Deleting dir: build")
-     shutil.rmtree("build")
+from setuptools import setup, find_packages
 
+#print(find_packages())
+#sys.exit()
 
-logger = logging.getLogger("Installer")
-logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-logger.addHandler(ch)
+'''packages = [ \
+    'add_source',
+    'answers_generator',
+    'auto_TL',
+    'build_all',
+    'checker_runner',
+    'checkers',
+    'cleaner',
+    'command_line',
+    'directory_diff',
+    'executors',
+    'export2ejudge',
+    'import_from_polygon',
+    'invoker',
+    'language',
+    'language_configurator',
+    'latex',
+    'package',
+    'reports',
+    'solution_runner',
+    'solution_tester',
+    'stress_tester',
+    'template',
+    'test_config_parser',
+    'tests_answer_generator',
+    'tests_generator',
+    'todo',
+    'utils',
+    'validator_runner'
+]'''
 
+#packages = ['please'] + ['please/' + s for s in packages]    
 
-if len(sys.argv) == 3 :
-     path = sys.argv[2]
-else:
-     system = platform_detector.get_platform()
-     print(system)
-     if   system[0] == "Windows":
-          path = os.path.join("C:\\\\Program Files","Please")
-          make_dir(path)
-          copy_file("please.py",os.path.join(path, "please.py"))
-     elif system[0] == 'Linux':
-          path = os.path.join("","usr","bin","please")
-          make_dir(path)
-          copy_file("please.py",os.path.join(path, "please"))
-     elif system[0] == 'Darwin':
-          path = os.path.join("/Application","Please")
-          make_dir(path)
-          copy_file("please.py",os.path.join(path, "please.py"))
-     else:
-          print("Your platform is unsupported")
-          exit()
-     
-logger.info("Copying finished!")
-logger.info("Setup library Please in Python")
+modules = [ \
+    'please',
+    'please/tags',
+    'please/globalconfig',
+    'please/log'
+]
 
-setup(name        = 'Please',
-      version     = '0.1',
-      description = '***',
-      packages = ['please'] + glob.glob("please/*/"),
-      package_data={'please': ['templates/*.*']}
-      )
+package_data = {
+    'please': ['templates/*.*', 'checkers/*.*']
+}
 
-delete_garbage()
-logger.info("Installation finished!")
+entry_points = {
+    'console_scripts' : ['please = please:main']
+}
+
+setup_params = { \
+    'name'             : 'Please', 
+    'version'          : '0.1', 
+    'description'      : '***', 
+    'py_modules'       : modules, 
+    'package_dir'      : {'please': 'please'}, 
+    'packages'         : find_packages(),
+    'package_data'     : package_data,
+    'install_requires' : ['lxml'],
+    'entry_points'     : entry_points
+}
+
+    
+
+setup(**setup_params)
