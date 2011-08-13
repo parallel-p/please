2f75a0de837aa7378ff9229cf81d31cf3e3114a8
dubov94@gmail.com <dubov94@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2011-08-13 01:14:56 +0000

    log rm fix

--- please/executors/trash_remover.py
+++ please/executors/trash_remover.py
@@ -8,10 +8,10 @@ def remove_trash (diff, is_trash):
 
 
 def remove_logs_in_depth(logs_names_list, base_dir):            
-    logger = logging.getLogger("please_logger.executors.remove_logs_in_depth")
+    logging.shutdown()
     for root, dirs, files in os.walk(base_dir):
         for file in files:
             for log in logs_names_list:
                 if file == log:
-                    logger.info("Removing log " + log + " in " + root)
+                    print("Removing log " + log + " in " + root)
                     os.remove(os.path.join(root, log))
