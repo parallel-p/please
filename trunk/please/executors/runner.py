ce32957a36a97ab2f08653fe1e18fa4f816797de
a.max95@gmail.com <a.max95@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2011-08-19 23:52:34 +0000

    alpha version of true runner

--- trunk/please/executors/runner.py
+++ trunk/please/executors/runner.py
@@ -2,11 +2,12 @@ from ..directory_diff import snapshot
 from ..invoker import invoker
 from ..language_configurator.lang_conf import get_language_configurator
 import psutil
+import tempfile
 from subprocess import PIPE
 from .. import globalconfig
 from . import trash_remover
 import threading
-import logging
+from please.log import logger
 import random
 import os
 
@@ -17,7 +18,26 @@ def __temp_file_name():
     name = 'tmp_' + str(random.randint(0, 1<<30))
     while os.path.exists(name):
         name = 'tmp_' + str(random.randint(0, 1<<30))
-    return name    
+    return name
+
+class ExecutionControl:
+    def __init__(self, stdin_fh, stdout_fh, stderr_fh, process):
+        self.stdin_fh = stdin_fh
+        self.stdout_fh = stdout_fh
+        self.stderr_fh = stderr_fh
+        self.process = process
+
+    def __enter__(self):
+        for f in (self.stdin_fh, self.stdout_fh, self.stderr_fh, self.process):
+            if (not f is None):
+                f.__enter__()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        b = True
+        for f in (self.stdin_fh, self.stdout_fh, self.stderr_fh, self.process):
+            if not f is None:
+                b = b or (f.__exit__(type, value, traceback) == True)
 
 def run(source, args_list = [], limits=globalconfig.default_limits, stdin_fh = None, \
         stdout_fh = None, stderr_fh = None, env=None, encoding = 'utf-8', shell = False):
@@ -38,62 +58,53 @@ def run(source, args_list = [], limits=globalconfig.default_limits, stdin_fh = N
         run("C:\\New Folder\\a.cpp", ["1", "3"], invoker.ExecutionLimits(10, 256), \
             file_handler, env = {"PATH": "C:\\Python32"})
     """
-    log = logging.getLogger("please_logger.executors.runner.run")
+
     snapshot_before = snapshot.Snapshot()
- 
-    tmp_stdout = None
-    tmp_stderr = None
-        
+
+    nrout = False;
+    nrerr = False;
+
     if stdout_fh is None:
-        tmp_stdout = __temp_file_name()
-        log.debug('creating stdout_fh %s', tmp_stdout)
-        stdout_fh = open(tmp_stdout, 'wb')
-            
+        nrout = True
+        stdout_fh = tempfile.TemporaryFile()
+
     if stderr_fh is None:
-        tmp_stderr = __temp_file_name()
-        log.debug('creating stderr_fh %s', tmp_stderr)
-        stderr_fh = open(tmp_stderr, 'wb')
-    
+        nrerr = True
+        stderr_fh = tempfile.TemporaryFile()
+
     lang = get_language_configurator(source)
     cmd = lang.get_run_command(source)
     args = cmd + args_list
-    log.debug("Starting process: args:%s, stdout:%s, stdin:%s, stderr:%s, env:%s", str(args), str(stdout_fh), str(stdin_fh), str(stderr_fh), str(env))   
-    while True:
-        try:
-            process = psutil.Popen(args, stdout = stdout_fh, stdin = stdin_fh, stderr = stderr_fh, env = env, shell = shell)
-            break
-        except psutil.error.NoSuchProcess:
-            log.info("NoSuchProcess error, trying again...")
-        except Exception as e:
-            log.error("Unknown exception while starting the process: %s", str(e))
-            break
-            
+    logger.debug("Starting process: args:%s, stdout:%s, stdin:%s, stderr:%s, env:%s", str(args), str(stdout_fh), str(stdin_fh), str(stderr_fh), str(env))
 
-    result = invoker.invoke(process, limits)
+    stdout = stderr = ''
+    process = psutil.Popen(args, stdout = stdout_fh, stdin = stdin_fh, stderr = stderr_fh, env = env, shell = shell)
+    result = None
+    try:
+        with ExecutionControl(stdin_fh, stdout_fh, stderr_fh, process) as ec:
+            result = invoker.invoke(process, limits)
+
+            snapshot_after = snapshot.Snapshot()
+
+            trash_remover.remove_trash(snapshot.get_changes(snapshot_before, snapshot_after), \
+                               lang.is_compile_garbage)
+            if nrout:
+                stdout_fh.seek(0)
+                stdout = stdout_fh.read();
+
+            if nrerr:
+                stderr_fh.seek(0)
+                stderr = stderr_fh.read();
+
+    except psutil.error.NoSuchProcess:
+        logger.error("NoSuchProcess error, fail...")
+    #except Exception as e:
+    #    logger.error("Unknown exception while invoking the process: %s", str(e))
+
     #print('\n'.join([c for a,b,c in os.walk('.')][0]))
-    snapshot_after = snapshot.Snapshot()
-    #log.debug('snapshot after: \n%s', str(snapshot_before))
-    log.debug("Removing trash created...")
-    
 
-    trash_remover.remove_trash(snapshot.get_changes(snapshot_before, snapshot_after), \
-                               lang.is_compile_garbage)        
-    if tmp_stdout is None:
-        stdout = None
-    else:
-        if not stdout_fh.closed:
-            stdout_fh.close()
-        with open(tmp_stdout, 'rb') as stdout_fh:
-            stdout = stdout_fh.read()
-        os.remove(tmp_stdout)
-     
-    if tmp_stderr is None:
-        stderr = None
-    else:
-        if not stderr_fh.closed:
-            stderr_fh.close()
-        with open(tmp_stderr, 'rb') as stderr_fh:
-            stderr = stderr_fh.read()
-        os.remove(tmp_stderr)
-        
+
+    logger.debug("Removing trash created...")
+
+
     return (result, stdout, stderr)
