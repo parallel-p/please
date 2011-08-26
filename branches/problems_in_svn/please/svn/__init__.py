f983e07e4cabb254e3f12f542ba4df739ef53377
gurovic@gmail.com <gurovic@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2011-08-26 21:32:34 +0000

    directory name includes username; directory problems is created if needed'

--- branches/problems_in_svn/please/svn/__init__.py
+++ branches/problems_in_svn/please/svn/__init__.py
@@ -6,26 +6,32 @@ from ..log import logger
 from ..globalconfig import svn, default_limits
 from ..invoker.invoker import invoke, ExecutionLimits
 
+def get_svnname(shortname):
+    user = svn['username'].split('@')[0]
+    return shortname + '_' + user 
+
 def svn_operation(command):
     handler = psutil.Popen(['svn'] + command + ['--username', svn['username'], 
                                       '--password', svn['password']],
                            stdout = PIPE) 
+    print(" ".join(['svn'] + command + ['--username', svn['username'], 
+                                      '--password', svn['password']]))
     result = invoke(handler, default_limits)
-    logger.info(" ".join(command)) 
-
-
+    if result.verdict == 'OK':
+        logger.info(" ".join(command)) 
+    return result
 
 def add_created_problem(shortname):
     shortname = str(shortname)
+    svnname = get_svnname(shortname)
 
     # 1. Create empty directory in svn
-    svn_operation(['mkdir', 
-                   svn['url'] + shortname,
+    # --parents creates directory 'problems', if it doesn't exist
+    svn_operation(['mkdir', svn['url'] + svnname, '--parents',
                    '-m', 'create directory for new problem ' + shortname])
 
     # 2. Checkout it into local problem directory
-    svn_operation(['checkout', 
-                   svn['url'] + shortname, shortname])
+    svn_operation(['checkout', svn['url'] + svnname, shortname])
 
     # 3. Add all content of this directory to svn
     chdir(shortname)
