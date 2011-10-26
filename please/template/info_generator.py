91175bd84f41895683e1d051a951bb3c054eaf69
gurovic@gmail.com <gurovic@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2011-10-26 11:45:18 +0000

    new todo engine, based on md5

--- please/template/info_generator.py
+++ please/template/info_generator.py
@@ -1,5 +1,6 @@
 import os
 import time
+import hashlib
 
 
 def create_time_file(root_path):
@@ -16,3 +17,24 @@ def create_time_file(root_path):
 	time_sec = str(time.time())
 	time_file.write(time_sec)
 	time_file.close()
+
+def create_md5_file(root_path):
+    '''
+    Creates md5conf.py with md5-sums of initial files
+    '''
+    md5file = open(os.path.join(root_path, '.please', 'md5.config'), 'w')
+    d = dict(checker='checker.cpp', 
+                   validator='validator.cpp', 
+                   statement=os.path.join('statements', 'default.ru.tex'),
+                   description=os.path.join('statements', 'description.ru.tex'),
+                   analysis=os.path.join('statements', 'analysis.ru.tex'),
+                   tests_description='tests.please',
+                   main_solution=os.path.join('solutions', 'solution.cpp')
+                  )
+    for (resource, dest) in d.items():
+        filepath = os.path.join(root_path, dest) 
+        if os.path.exists(filepath):
+            m = hashlib.md5()
+            m.update(open(filepath,'r+b').read())
+            md5file.write(resource + ":" + m.hexdigest() + "\n")
+    md5file.close()
