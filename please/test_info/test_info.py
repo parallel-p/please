fe4bef59737b7789c12e5e050515670d078fc319
gurovic@gmail.com <gurovic@users.noreply.github.com>
2011-12-29 06:06:46 +0000

    testinfo interface

---
+++ please/test_info/test_info.py
@@ -0,0 +1,13 @@
+class TestInfo:
+    def __init__():
+        raise NotImplementedError()
+    
+    def tests():
+        raise NotImplementedError()
+    
+    def get_tags():
+        raise NotImplementedError()
+    
+    def to_please_format():
+        raise NotImplementedError()
+    
