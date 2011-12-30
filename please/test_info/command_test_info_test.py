d73b50de29e60c7087af7e49b6bd032244a282b7
dubov94 <dubov94@cb711904-5dbb-0428-5097-0481b5fa17eb>
2011-12-30 14:27:05 +0000

    command_test_info is made

--- please/test_info/command_test_info_test.py
+++ please/test_info/command_test_info_test.py
@@ -3,12 +3,15 @@ from . import command_test_info
 import os
 
 class CommandTestInfoTest(unittest.TestCase):
+    
     def test_simple_echo(self):
         a = command_test_info.CommandTestInfo(["echo"], ["1", "2", "3"], {"G" : "gurda"} )
         res = a.tests()
         
         self.assertEqual(open(res[0], mode = 'rb').read(), b"1 2 3\n")
         os.remove(res[0])
+        
+        self.assertEqual(a.to_please_format(), "[G = gurda] echo 1 2 3")
         
 if __name__ == '__main__':
     unittest.main()
