cf7a45190cd83834d746e038c4c88807fde1278a
dubov94 <dubov94@cb711904-5dbb-0428-5097-0481b5fa17eb>
2011-12-30 11:59:38 +0000

    command_test_info

--- please/test_info/command_test_info_test.py
+++ please/test_info/command_test_info_test.py
@@ -4,10 +4,10 @@ import os
 
 class CommandTestInfoTest(unittest.TestCase):
     def test_simple_echo(self):
-        a = command_test_info.CommandTestInfo(["echo"], ["1", "2", "3"])#["1", "2", "3"])#["1", "2", "3"], {"G" : "gurda"} )
+        a = command_test_info.CommandTestInfo(["echo"], ["1", "2", "3"], {"G" : "gurda"} )
         res = a.tests()
         
-        self.assertEqual(open(res[0]).read(), "1 2 3")
+        self.assertEqual(open(res[0], mode = 'rb').read(), b"1 2 3\n")
         os.remove(res[0])
         
 if __name__ == '__main__':
