8f0fadbebb6f3b88ee8c51f5998e51f3797f9d7b
dubov94 <dubov94@cb711904-5dbb-0428-5097-0481b5fa17eb>
2011-12-30 08:21:57 +0000

    factory impl add

--- please/test_info/stdgen_test_info.py
+++ please/test_info/stdgen_test_info.py
@@ -2,7 +2,7 @@ from . import test_info
 
 class StdGenTestInfo(test_info.TestInfo):
     def __init__(self, generator, args, tags={}):
-        super(FileTestInfo, self).__init__(tags)
+        super(StdGenTestInfo, self).__init__(tags)
         self.__generator = generator
         self.__args = args
         
