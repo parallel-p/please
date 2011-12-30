30388270fe0f66fb5623f8d425e5a71a2600499f
dubov94 <dubov94@cb711904-5dbb-0428-5097-0481b5fa17eb>
2011-12-30 15:41:30 +0000

    cmd_stdgen_test_info is compl done

--- please/test_info/cmd_stdgen_test_info_test.py
+++ please/test_info/cmd_stdgen_test_info_test.py
@@ -12,6 +12,14 @@ class CmdOrStdGenTestInfo(unittest.TestCase):
         os.remove(res[0])
         
         self.assertEqual(a.to_please_format(), "[G = gurda] echo 1 2 3")
-    #TODO: add tests for real generators
+        
+    def test_generator(self):
+        stdir = os.getcwd()
+        a = cmd_stdgen_test_info.CmdOrStdGenTestInfo("test_problems/generator/main.cpp", ["blue", "dog"])
+        res = a.tests()
+        
+        self.assertEqual(open(res[0]).read(), "blue dog ")
+        os.remove(res[0])
+        
 if __name__ == '__main__':
     unittest.main()
