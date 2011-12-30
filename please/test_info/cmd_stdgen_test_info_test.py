21f01faa1c4cf5aed0a7f14becc426541db6f612
dubov94 <dubov94@cb711904-5dbb-0428-5097-0481b5fa17eb>
2011-12-30 15:29:02 +0000

    merged command's and stdgenerator's test info classes

--- please/test_info/cmd_stdgen_test_info_test.py
+++ please/test_info/cmd_stdgen_test_info_test.py
@@ -1,17 +1,17 @@
 import unittest
-from . import cmd_or_std_gen_test_info
+from . import cmd_stdgen_test_info
 import os
 
 class CmdOrStdGenTestInfo(unittest.TestCase):
     
     def test_simple_echo(self):
-        a = cmd_or_std_gen_test_info.CmdOrStdGenTestInfo("echo", ["1", "2", "3"], {"G" : "gurda"} )
+        a = cmd_stdgen_test_info.CmdOrStdGenTestInfo("echo", ["1", "2", "3"], {"G" : "gurda"} )
         res = a.tests()
         
         self.assertEqual(open(res[0]).read(), "1 2 3\n")
         os.remove(res[0])
         
         self.assertEqual(a.to_please_format(), "[G = gurda] echo 1 2 3")
-        
+    #TODO: add tests for real generators
 if __name__ == '__main__':
     unittest.main()
