0b19ab68f9476ed0255d156abd20addb89229474
oleg@mingalev.net <oleg@mingalev.net@cb711904-5dbb-0428-5097-0481b5fa17eb>
2011-12-31 09:54:36 +0000

    ziparchiver add_folder added

--- please/archiver/ziparchiver.py
+++ please/archiver/ziparchiver.py
@@ -1,4 +1,5 @@
 import zipfile
+import os
 
 class ZIPArchiver:
     '''
@@ -13,7 +14,15 @@ class ZIPArchiver:
     z.add('file5.txt', 'new_dir3/new_file5_name.txt')  #add file5.txt in new_dir3 as new_file5_name.txt
     z.add('file6.txt', 'new_dir4\\new_file6_name.txt')  #add file6.txt in new_dir4 as new_file6_name.txt
     z.add('file7.txt', 'new_dir5')                     #add file7.txt as new_dir5
+    z.add_folder('dir1')                               #add folder dir1 in root
+    z.add_folder('dir2', 'path1')                       #add folder dir2 in path1
     '''
+    def add_folder(self, directory, folder = ""):
+        for item in os.listdir(directory):
+            if os.path.isfile(os.path.join(directory, item)):
+                self.sbj.write(os.path.join(directory, item), folder + os.sep + item)
+            elif os.path.isdir(os.path.join(directory, item)):
+                self.add_folder(os.path.join(directory, item), folder + os.sep + item)
     def __init__(self, path, mod = 'w'):
         self.sbj = zipfile.ZipFile(path, mod)
     def add(self, src_path, dst_path = './'):
