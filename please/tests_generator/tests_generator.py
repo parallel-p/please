c1822bb22bcd3e9c24371b1f5c3eb3d16202989e
mischapanin@gmail.com <mischapanin@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2011-12-31 06:41:12 +0000

    adopted tests_generator to new test_info

--- please/tests_generator/tests_generator.py
+++ please/tests_generator/tests_generator.py
@@ -1,7 +1,7 @@
 import shutil
 import os
-from ..executors import compiler, runner
 from ..utils import line_ending
+from ..executors import compiler, runner
 from .. import globalconfig
 import io
 import logging
@@ -14,33 +14,46 @@ class TestsGenerator:
     def __init__(self, tests_info, prefix=""):
         self.__tests_info = tests_info
         self.__prefix = prefix
+    
+    def __generate_test(self, test, first_test_id):
         
+        
+        temp_file_names = test.tests()
+        file_names = []
+        tests_in_series_number = 0
+        for temp_file in temp_file_names:
+           
+            file_name = self.__prefix + "{0:d}".format(first_test_id + tests_in_series_number)
+            tests_in_series_number += 1
+            file_name = os.path.join(TESTS_DIR, file_name)     
+            shutil.move(temp_file, file_name)
+            file_names.append(file_name)
+        
+        return file_names, tests_in_series_number
+    
     def generate(self, admit):
         '''
         generates tests, whos tags admit given lambda
-        '''
-        if not os.path.exists(TESTS_DIR):
-            os.makedirs(TESTS_DIR)
+        '''        
+        if os.path.exists(TESTS_DIR):
+            shutil.rmtree(os.path.join(TESTS_DIR))        
+        os.makedirs(TESTS_DIR)
         generated = []
         
-        logger.info('Generating {0} tests'.format(len([test for test in self.__tests_info if admit(test.get_tags())])))
-        global_test_counter = 1
+        logger.info('Generating {0} tests'.format(len([test for test in self.__tests_info if admit(test.tags)])))
         
-        for test in self.__tests_info:
-            if (admit(test.get_tags())):
-                files = test.tests()
-                for file in files:
-                    new_file_name = self.__prefix + "{0:d}".format(global_test_counter)
-                    new_file_name = os.path.join(TESTS_DIR, new_file_name)
-                    shutil.move(file, new_file_name)
-                    #As TestInfo descendants give to us temporary files with tests, we should move 'em
-                    global_test_counter += 1
-                    generated.append(new_file_name)
-                    logger.info('Test #{0} generated'.format(global_test_counter))
+        current_test_id = 1
+        for i, test in enumerate(self.__tests_info):
+                        # TODO : this should be changed to generated_tests_count
+            if (admit(test.tags)):
+                file_names, tests_in_series_count = self.__generate_test(test, current_test_id)    
+                current_test_id += tests_in_series_count               
+                for file_name in file_names:
+                    generated.append(file_name)
+                logger.info('Test Series#{0} generated'.format(i + 1))
         
         return generated
         
     def generate_all(self):
-        if (os.path.exists(TESTS_DIR)):
-            shutil.rmtree(os.path.join(TESTS_DIR))
+        
         return self.generate(lambda x: True)
