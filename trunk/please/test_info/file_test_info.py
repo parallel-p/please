fe2dd18c2723566e7585f76dbda71abc7cc0b5f4
dubov94@gmail.com <dubov94@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2011-12-31 08:12:41 +0000

    1 failures left

--- trunk/please/test_info/file_test_info.py
+++ trunk/please/test_info/file_test_info.py
@@ -1,13 +1,16 @@
 from . import test_info
 import tempfile
 import shutil
+#from ..well_done.well_done import WellDone
 
 class FileTestInfo(test_info.TestInfo):
-    def __init__(self, filename, tags={}):
+    def __init__(self, well_done, filename, tags={}):
         self.__file = filename
+        self.__well_done = well_done
         super(FileTestInfo, self).__init__(tags)
     
     def tests(self):
+        self.__well_done.check(self.__file)
         temp = tempfile.NamedTemporaryFile(delete = False)
         shutil.copy(self.__file, temp.name)
         return [ temp.name ]
--- trunk/please/test_info/file_test_info_test.py
+++ trunk/please/test_info/file_test_info_test.py
@@ -4,14 +4,21 @@ import os
 
 class FileTestInfoTest(unittest.TestCase):
         
+    class WellDoneMock():
+        def __init__(self, check_functions_list):
+            self.__check_functions_list = check_functions_list
+        def check(self, filename):
+            return True
+    
     def test_one_big(self):
+        well_done = self.WellDoneMock(['no_symbols_less_32', 'no_left_right_space'])
         myfilename = "my.txt"
         
         pig = open(myfilename , "w")
         pig.write("test")
         pig.close()
         
-        fti = file_test_info.FileTestInfo("my.txt", {"to":"be", "or":"not"})
+        fti = file_test_info.FileTestInfo(well_done, "my.txt", {"to":"be", "or":"not"})
         r = fti.tests()
         
         self.assertDictEqual(fti.get_tags(), {"to":"be", "or":"not"})
