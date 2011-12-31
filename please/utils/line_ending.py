b8af9f6cd0e7df0a2f1bd429d126a7a3feee8fb2
mischapanin@gmail.com <mischapanin@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2011-12-31 06:40:26 +0000

    added comment

--- please/utils/line_ending.py
+++ please/utils/line_ending.py
@@ -1,4 +1,7 @@
 def convert(file):
+    """
+    This method converts all ends of lines in file to utf-8 styled ends of file, using dirty hack
+    """
     data = None
     with open(file, 'r', encoding='utf-8') as f:
         data = f.read()
