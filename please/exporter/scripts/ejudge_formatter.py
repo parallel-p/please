a95542294d9ed5bab9a6bdd30876b2baf0663a2c
PyshkinIgor@gmail.com <PyshkinIgor@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2011-12-31 07:13:11 +0000

    made NewEjudgeFormatter without backup

--- please/exporter/scripts/ejudge_formatter.py
+++ please/exporter/scripts/ejudge_formatter.py
@@ -6,33 +6,37 @@ class EjudgeFormatter:
         raise NotImplementedError
         
 class NewEjudgeFormatter(EjudgeFormatter):
-    def __init__(self, dictionary):
-        self.__dictionary = dictionary
+    def __init__(self, problems):
+        # problems is a list of structures:
+        # it has three parameters:
+        # 1) problem_from - the shortname of this problem
+        # 2) problem_to - the target shortname in the Ejudge of this problem
+        # 3) problem_checker - the name of checker.
+        self.__problems = problems
 
     def put_all(self):
         target_path_to_problems = os.path.join('..', 'problems')
         if not os.path.exists(target_path_to_problems):
             os.mkdir(target_path_to_problems)
 
-        names_of_problems = os.listdir('.') 
-        for name_of_problem in names_of_problems:                     
-            path_to_problem = os.join('.', name_of_problem)
-            if not os.path.isdir(path_to_problem)
-                continue
-            target_path_to_problem = os.join(target_path_to_problems, self.__dictionary[name_of_problem])
+        for problem in self.__problems:                     
+            path_to_problem = os.join('.', problem.problem_from)
+            if not os.path.isdir(path_to_problem):
+                raise FileNotFoundException
+                
+            target_path_to_problem = os.join(target_path_to_problems, problem.problem_to)
             if os.path.exists(target_path_to_problem):
                 # make backup!
                 shutil.rmtree(target_path_to_problem)
             os.mkdir(target_path_to_problem)
             put_tests(path_to_problem, target_path_to_problem)
-            put_checker(path_to_problem, target_path_to_problem)
+            put_checker(path_to_problem, target_path_to_problem, problem.problem_checker)
                         
 
     def put_tests(self, path_to_problem, target_path_to_problem):
-        os.mkdir(io.path.join(target_path_to_problem, 'tests'))
-        assert (os.path.isdir(io.path.join(path_to_problem, '.tests'))):
-        shutil.copytree(io.path.join(path_to_problem, '.tests'), io.path.join(target_path_to_problem, 'tests'))        
+        os.mkdir(os.path.join(target_path_to_problem, 'tests'))
+        assert (os.path.isdir(os.path.join(path_to_problem, '.tests')))
+        shutil.copytree(os.path.join(path_to_problem, '.tests'), os.path.join(target_path_to_problem, 'tests'))        
     
-    def put_checker(self, path_to_problem, target_path_to_problem):
-        pass
-    
+    def put_checker(self, path_to_problem, target_path_to_problem, name_of_checker):
+        shutil.copyfile(os.path.join(path_to_problem, name_of_checker), os.path.join(target_path_to_problem, name_of_checker))
