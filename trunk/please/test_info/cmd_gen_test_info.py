85aa9771ffe88b9be4b94f508b9638fc29a4c66e
tigvarts@gmail.com <tigvarts@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2011-12-31 09:48:20 +0000

    updated to use multigenerators, generator and commands in one class, fixed tests

---
+++ trunk/please/test_info/cmd_gen_test_info.py
@@ -0,0 +1,38 @@
+from please.test_info import test_info
+import tempfile
+from ..executors import runner, compiler
+from please.diff_test_finder.diff_test_finder import DiffTestFinder
+import please.directory_diff.snapshot as snapshot
+import os
+
+class CmdOrGenTestInfo(test_info.TestInfo):
+    def __init__(self, executor, args, tags={}):
+        """examples: command = "echo", args = ["1", "2", "3"]
+        command = "generator.cpp", args = ["17", "42", "100500"]
+        """
+        self.__executor = executor
+        self.__args = args
+        super(CmdOrGenTestInfo, self).__init__(tags)
+        
+    def tests(self):
+        problem_dir = os.getcwd() #really?
+        
+        stdout = tempfile.NamedTemporaryFile(delete = False)
+        compiler.compile(self.__executor)
+
+        snapshot_before = snapshot.Snapshot(problem_dir)
+        runner.run(self.__executor, self.__args, stdout_fh = stdout)       
+        snapshot_after = snapshot.Snapshot(problem_dir, files_to_ignore = [stdout.name])
+        
+        diff = snapshot.get_changes(snapshot_before, snapshot_after)
+        
+        exe_dir = os.getcwd() #really?
+        mask = self.get_tags().get('mask')
+        exclude = self.get_tags().get('exclude')
+        diff_test_finder = DiffTestFinder(exe_dir, mask, exclude)
+        tests = diff_test_finder.tests(diff, stdout.name)
+        
+        return tests
+    
+    def to_please_format(self):
+        return self.get_to_please_format_prefix() + self.__executor + " " + " ".join(self.__args)
