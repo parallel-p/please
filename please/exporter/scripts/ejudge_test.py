4d04c328ea950d802f7105a2a24ca049ff92984a
zhuravlevilya1@gmail.com <zhuravlevilya1@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-01-01 11:26:56 +0000

    Added mocks to ejudge_test.py

--- please/exporter/scripts/ejudge_test.py
+++ please/exporter/scripts/ejudge_test.py
@@ -1,12 +1,26 @@
 import please.exporter.scripts.ejudge as ejudge_exporter
+import please.exporter.scripts.ejudge_formatter as ejudge_formatter
 
-import unittest, os
+import unittest, os, mox
+
+class FakeFormatter():
+    def __init__(self):
+        pass
+    def put_all(self):
+        pass
 
 class CppTest(unittest.TestCase):
     def setUp(self):
+        self.mox = mox.Mox()
         self.old_cwd = os.getcwd()
 
     def test_ejudge(self):
+        self.mox.StubOutWithMock(ejudge_formatter, 'EjudgeFormatter')
+        self.mox.StubOutWithMock(ejudge_formatter, 'NewEjudgeFormatter')
+        ejudge_formatter.EjudgeFormatter(mox.IgnoreArg()).AndReturn(FakeFormatter())
+        ejudge_formatter.NewEjudgeFormatter(mox.IgnoreArg()).AndReturn(FakeFormatter())
+        self.mox.ReplayAll()
+
         cur_root = os.path.split(__file__)[0]
         os.chdir(os.path.join(cur_root, "tests", "please_tmp"))
         ejudge_exporter.export('../conf/serve.cfg', '../conf/serve.cfg.out')
@@ -18,6 +32,7 @@ class CppTest(unittest.TestCase):
             self.assertEqual(out_lines[x], corr_lines[x])
 
     def tearDown(self):
+        self.mox.UnsetStubs()
         os.chdir(self.old_cwd)
 
 if __name__ == '__main__':
