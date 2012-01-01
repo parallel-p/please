e76e47b8fe982fe0b732cc8464e1188f2de9b93d
mischapanin@gmail.com <mischapanin@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-01-01 14:15:37 +0000

    changed tests to new parser

--- trunk/please/test_config_parser/parser_test.py
+++ trunk/please/test_config_parser/parser_test.py
@@ -7,7 +7,15 @@ import os
 class TestObjectFactoryTest(unittest.TestCase):
     def setUp(self):
         self.mox = mox.Mox()
+    
+    class WellDoneMock():
+        def __init__(self, key):
+            self.__key = key
+        def check(self, file):
+            return 0  
+			        
     def test_command(self):
+        well_done = self.WellDoneMock("key")
         t = parser.TestObjectFactory.create(1, "echo",  ["17",  "mama"])
         self.assertIsInstance(t, cmd_gen_test_info.CmdOrGenTestInfo)
         q = t.tests()
@@ -16,23 +24,27 @@ class TestObjectFactoryTest(unittest.TestCase):
         os.remove(q[0])
         
     def test_stdgen(self):
+        well_done = self.WellDoneMock("key")
         self.assertIsInstance(
-            parser.TestObjectFactory.create(1, "gen.cpp", ["17", "42", "100500"]), cmd_gen_test_info.CmdOrGenTestInfo)
+            parser.TestObjectFactory.create(well_done, 1, "gen.cpp", ["17", "42", "100500"]), cmd_gen_test_info.CmdOrGenTestInfo)
     def test_nonexist_file(self):
+        well_done = self.WellDoneMock("key")
         with self.assertRaises(EnvironmentError):
-            parser.TestObjectFactory.create(1, "iwasbreaking.awindow", [])
+            parser.TestObjectFactory.create(well_done, 1, "iwasbreaking.awindow", [])
             
     def test_exist_file(self):
         self.mox.StubOutWithMock(os.path, "exists")
         os.path.exists(os.path.join(".","test.txt")).AndReturn(True)
         self.mox.ReplayAll()
-        
-        a = parser.TestObjectFactory.create(1, "test.txt", [], {"a":"17", "b":"c"})
+
+        well_done = self.WellDoneMock("key")        
+        a = parser.TestObjectFactory.create(well_done, 1, "test.txt", [], {"a":"17", "b":"c"})
         self.assertIsInstance(a, file_test_info.FileTestInfo)
         self.assertDictEqual(a.get_tags(), {"a":"17", "b":"c"})
         
     def test_parse(self):
-        a = parser.TestConfigParser("test_problems/generator/main.cpp 1 2 3\n[sample]test_problems/generator/main.cpp happy new year")
+        well_done = self.WellDoneMock("key")
+        a = parser.TestConfigParser(well_done, "test_problems/generator/main.cpp 1 2 3\n[sample]test_problems/generator/main.cpp happy new year")
         self.assertEqual(a.get_binaries(), [os.path.join("test_problems", "generator", "main.cpp")])
         b = a.get_test_info_objects()
         
