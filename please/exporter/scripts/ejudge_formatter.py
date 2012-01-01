752031ea5d325d4b869741ff25e31dff6fc8af9e
PyshkinIgor@gmail.com <PyshkinIgor@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-01-01 13:09:46 +0000

    made these modules workable

--- please/exporter/scripts/ejudge_formatter.py
+++ please/exporter/scripts/ejudge_formatter.py
@@ -16,6 +16,13 @@ class NewEjudgeFormatter(EjudgeFormatter):
         # 3) problem_checker - the name of checker.
         self.__problems = problems
 
+    def __put_tests(self, path_to_problem, target_path_to_problem):
+        assert (os.path.isdir(os.path.join(path_to_problem, '.tests')))
+        shutil.copytree(os.path.join(path_to_problem, '.tests'), os.path.join(target_path_to_problem, 'tests'))        
+    
+    def __put_checker(self, path_to_problem, target_path_to_problem, name_of_checker):
+        shutil.copyfile(os.path.join(path_to_problem, name_of_checker), os.path.join(target_path_to_problem, name_of_checker))
+
     def put_all(self):
         target_path_to_problems = os.path.join('..', 'problems')
         if not os.path.exists(target_path_to_problems):
@@ -28,24 +35,25 @@ class NewEjudgeFormatter(EjudgeFormatter):
                 
             target_path_to_problem = os.path.join(target_path_to_problems, problem.problem_to)
             if os.path.exists(target_path_to_problem):
-                make_backup()
                 shutil.rmtree(target_path_to_problem)
             os.mkdir(target_path_to_problem)
-            put_tests(path_to_problem, target_path_to_problem)
-            put_checker(path_to_problem, target_path_to_problem, problem.problem_checker)
+            self.__put_tests(path_to_problem, target_path_to_problem)
+            self.__put_checker(path_to_problem, target_path_to_problem, problem.problem_checker)
                         
-
-    def put_tests(self, path_to_problem, target_path_to_problem):
-        os.mkdir(os.path.join(target_path_to_problem, 'tests'))
-        assert (os.path.isdir(os.path.join(path_to_problem, '.tests')))
-        shutil.copytree(os.path.join(path_to_problem, '.tests'), os.path.join(target_path_to_problem, 'tests'))        
-    
-    def put_checker(self, path_to_problem, target_path_to_problem, name_of_checker):
-        shutil.copyfile(os.path.join(path_to_problem, name_of_checker), os.path.join(target_path_to_problem, name_of_checker))
 
 class OldEjudgeFormatter(EjudgeFormatter):
     def __init__(self):
         raise NotImplementedError
     def put_all(self):
         raise NotImplementedError
-    
+
+'''
+class MyProblem:
+    def __init__(self, problem_from, problem_to, problem_checker):
+        self.problem_from = problem_from
+        self.problem_to = problem_to
+        self.problem_checker = problem_checker
+
+NEF = NewEjudgeFormatter([MyProblem('a', 'abacaba', 'checkernew.cpp')])
+NEF.put_all()
+'''
