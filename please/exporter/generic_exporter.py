8a6e950079f8ba5265a58964eaa1d560a0d348a6
oleg@mingalev.net <oleg@mingalev.net@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-01-02 11:11:59 +0000

    fxd

--- please/exporter/generic_exporter.py
+++ please/exporter/generic_exporter.py
@@ -1,8 +1,11 @@
 import shutil
 import os
+from ..package.config import Config
 from ..command_line.generate_tests import generate_tests
 from ..ssh_tools.connector import Connector
 from ..build_all.build_tools import build_all
+from please.checkers.standard_checkers_utils import add_standard_checker_to_solution
+
 class GenericExporter:
     def __init__(self,archiver = None, connector = None,libs = [],problems = []):
         self.archiver = archiver
@@ -12,6 +15,10 @@ class GenericExporter:
         for problem in self.problems:
             os.chdir(problem)
             generate_tests()
+            with open('default.packahe', 'r') as f:
+                conf = Config(f.read())
+            if not os.path.exists(conf['checker']):
+                add_standart_checker_to_solution(conf['checker'])
             os.chdir('..')
             self.archiver.add_folder(problem, problem)
     
