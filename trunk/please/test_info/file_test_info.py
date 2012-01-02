db2923906c7d6cf749d89a1cf1022064b6b8e77b
dubov94@gmail.com <dubov94@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-01-02 10:53:24 +0000

    added file mask test selector functionality

--- trunk/please/test_info/file_test_info.py
+++ trunk/please/test_info/file_test_info.py
@@ -1,20 +1,24 @@
 from . import test_info
 import tempfile
 import shutil
+import glob
 from ..well_done import well_done
 
 class FileTestInfo(test_info.TestInfo):
-    def __init__(self, filename, tags={}, well_done=None):
-        self.__file = filename
+    def __init__(self, mask, tags={}, well_done=None):
+        self.__mask = mask
         self.__well_done = well_done
         super(FileTestInfo, self).__init__(tags)
     
     def tests(self):
-        if self.__well_done is not None:
-            self.__well_done.check(self.__file)
-        temp = tempfile.NamedTemporaryFile(delete = False)
-        shutil.copy(self.__file, temp.name)
-        return [ temp.name ]
+        result = []
+        for file in glob.iglob(self.__mask):
+            if self.__well_done is not None:
+                self.__well_done.check(file)
+            temp = tempfile.NamedTemporaryFile(delete = False)
+            shutil.copy(file, temp.name)
+            result.append(temp.name)
+        return result
     
     def to_please_format(self):
         return self.get_to_please_format_prefix() + self.__file
