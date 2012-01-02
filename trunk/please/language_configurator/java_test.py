cb7d91e4ba7961d7fd647c11a942a0d76b7ccc58
VitalyGoldstein <VitalyGoldstein@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-01-02 15:17:02 +0000

    fix java configurator

--- trunk/please/language_configurator/java_test.py
+++ trunk/please/language_configurator/java_test.py
@@ -6,32 +6,35 @@ class JavaTest(unittest.TestCase):
     def test_win_compile(self):
         java = javaconf.JavaWindowsConfigurator()
         out = java.get_compile_command("test.java")
-        self.assertEqual(out, ["javac", "-cp", ".;", "test.java"])
+        self.assertEqual(out, ["javac", "-d", ".please", "-cp", ".;", "test.java"])
         
     def test_win_run1(self):
         java = javaconf.JavaWindowsConfigurator()
         out = java.get_run_command("abacaba.java")
-        self.assertEqual(out, ["java", "-cp", os.path.dirname(os.path.abspath("abacaba")) ,"abacaba"])
+        self.assertEqual(out, ["java", "-cp", ".please", "abacaba"])
         
     def test_win_run2(self):
         java = javaconf.JavaWindowsConfigurator()
         out = java.get_run_command("aba/caba.java")
-        self.assertEqual(out, ["java", "-cp", os.path.dirname(os.path.abspath("aba/caba")), "caba"])
+        self.assertEqual(out, ["java", "-cp", ".please", "caba"])
         
     def test_linux_compile(self):
         java = javaconf.JavaLinuxConfigurator()
         out = java.get_compile_command("test.java")
-        self.assertEqual(out, ["javac" ,"-cp", ".;", "test.java"])
+        self.assertEqual(out, ["javac" ,"-d", ".please", "-cp", ".;", "test.java"])
         
     def test_linux_run(self):
         java = javaconf.JavaLinuxConfigurator()
         out = java.get_run_command("abacaba.java")
-        self.assertEqual(out, ["java", "-cp", os.path.dirname(os.path.abspath("abacaba")),"abacaba"])
+        self.assertEqual(out, ["java" ,"-cp", ".please", "abacaba"])
         
     def test_linux_run2(self):
         java = javaconf.JavaLinuxConfigurator()
         out = java.get_run_command("aba/caba.java")
-        self.assertEqual(out, ["java", "-cp", os.path.dirname(os.path.abspath("aba/caba")), "caba"])
+        self.assertEqual(out, ["java", "-cp", ".please", "caba"])
+
+    #TODO: add tests for is_compile_garbage and get_binary_name
+    #TODO: add tests for compiling in another dir
         
 if __name__ == '__main__':
     unittest.main()
