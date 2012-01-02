f0e9b4291b8f775dcbaa8a223e41f6a9c0950666
PyshkinIgor@gmail.com <PyshkinIgor@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-01-02 08:00:36 +0000

    deleted echo test.

--- please/test_info/cmd_gen_test_info_test.py
+++ please/test_info/cmd_gen_test_info_test.py
@@ -5,17 +5,6 @@ import shutil
 
 class CmdOrGenTestInfo(unittest.TestCase):
     
-    def test_simple_echo(self):
-        a = cmd_gen_test_info.CmdOrGenTestInfo("echo", ["1", "2", "3"], {"G" : "gurda"} )
-        res = a.tests()
-       
-        #TODO:what is res[0]? 
-        with open(res[0]) as res_file:
-            self.assertEqual(res_file.read(), "1 2 3\n")
-        os.remove(res[0])
-        
-        self.assertEqual(a.to_please_format(), "[G = gurda] echo 1 2 3")
-        
     def test_generator(self):
         stdir = os.getcwd()
         a = cmd_gen_test_info.CmdOrGenTestInfo("test_problems/generator/main.cpp", ["blue", "dog"])
