f2f11e1f3848b10614de25f798a6a1dbb9e89195
dubov94@gmail.com <dubov94@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-01-02 09:29:20 +0000

    improve for validator errors

--- please/utils/form_error_output.py
+++ please/utils/form_error_output.py
@@ -10,6 +10,5 @@ def process_err_exit(err_str, verd, ex_code, stdout, stderr, logger):
     out_err_str = err_str + " " + verd
     if verd == "RE":
         out_err_str += " with exit code: " + str(ex_code)
-    logger.error(out_err_str)
-    formed_std = form_err_string_by_std(stdout, stderr)
-    if formed_std != "": logger.error(formed_std)
+    out_err_str += form_err_string_by_std(stdout, stderr)
+    return out_err_str
