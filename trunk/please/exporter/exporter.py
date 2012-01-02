564fd4239f1b082981ac7b0cc15b9de74cbaab2c
oleg@mingalev.net <oleg@mingalev.net@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-01-02 07:02:09 +0000

    export & ejudge_export wasn't done but now

---
+++ trunk/please/exporter/exporter.py
@@ -0,0 +1,8 @@
+from please import globalconfig
+
+def export(server_name, problems):
+    server = globalconfig.servers[server_name]
+    server.set_problems(problems)
+    server.create_archive()
+    server.upload_file()
+    server.run_script()
