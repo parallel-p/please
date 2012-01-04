2f6991891d1412893ba8a4191546bf6a16e5f95b
tigvarts@gmail.com <tigvarts@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-01-04 03:28:22 +0000

    added file

---
+++ please/solution_tester/solution_config_utils.py
@@ -0,0 +1,42 @@
+import os.path
+from ..solution_tester.package_config import PackageConfig
+from .. import globalconfig
+from ..invoker import invoker
+
+def make_config_with_solution_config(config, solution_config):
+    # Get results from test_solution, create a config file to send.
+    # Find all attributes from config's root or embedded solution's config        
+    new_config = {}
+    new_config["checker"] = config["checker"]
+    new_config["tests_dir"] = globalconfig.temp_tests_dir #config["tests_dir"]        
+    new_config["execution_limits"]  = invoker.ExecutionLimits(float(config["time_limit"]), float(config["memory_limit"]))
+    new_config["solution_config"] = {"input":config["input"], "output":config["output"]}
+    new_config["solution_args"] = [] #looks like solution needs no argument
+    
+    if solution_config is not None:
+        #print("SOLUTION FOUND: " + sol_found["source"])               
+        new_config["expected_verdicts"] = solution_config.get("expected_verdicts", [])
+        new_config["optional_verdicts"] = solution_config.get("possible_verdicts", [])
+        if "input" in solution_config:
+            new_config["solution_config"]["input"]  = solution_config["input"]
+        if "output" in solution_config:
+            new_config["solution_config"]["output"] = solution_config["output"]
+#        solution = os.path.abspath(sol_found["source"])
+    else:
+        pass
+#        raise SolutionNotFoundException(solution + ' not found in config')
+
+    return new_config
+        
+def make_config(solution, config = None):
+    if config == None:
+        config = PackageConfig.get_config()
+        
+    solution_config = None
+    for cur_solution_config in config["solution"]:
+        if cur_solution_config["source"] == os.path.relpath(solution):
+#        if os.path.normpath(solution) in os.path.abspath(sol_found["source"]):
+            solution_config = cur_solution_config
+    
+    return make_config_with_solution_config(config, solution_config)
+
