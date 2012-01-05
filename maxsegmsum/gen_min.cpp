a3ea0ebf1beb27955ec000ca428c3d1903b57d22
PyshkinIgor@gmail.com <PyshkinIgor@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-01-05 11:04:10 +0000

    make problem to show

---
+++ maxsegmsum/gen_min.cpp
@@ -0,0 +1,57 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <cassert>
+#include <ctime>
+#include "testlib.h"
+
+using namespace std;
+
+#ifdef WIN32
+    #define LLD "%I64d"
+#else
+    #define LLD "%lld"
+#endif
+
+typedef pair<int, int> pii;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+#define pb push_back
+#define mp make_pair
+#define EPS (1e-9)
+#define INF ((int)1e9)
+#define sqr(x) ((x) * (x))         
+
+void writeTest(int test)
+{
+    startTest(test);
+    int n = rnd.next(1, (int)10);
+    cout << n << endl;
+    for (int i = 0; i < n; i++) {
+        int x = rnd.next(1, (int)1e9);
+        printf("%d%c", x, " \n"[i == n - 1]);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    registerGen(argc, argv);
+
+    for (int i = 1; i <= 40; i++)
+        writeTest(i);
+    
+    return 0;
+}
