714ecf14761537cc8a69849ed0a64f86617a06f0
VitalyGoldstein <VitalyGoldstein@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-01-05 10:21:06 +0000

    fix java compiling

--- trunk/please/language_configurator/java_test.py
+++ trunk/please/language_configurator/java_test.py
@@ -6,32 +6,32 @@ class JavaTest(unittest.TestCase):
     def test_win_compile(self):
         java = javaconf.JavaWindowsConfigurator()
         out = java.get_compile_command("test.java")
-        self.assertEqual(out, ["javac", "-d", ".please", "-cp", ".", "test.java"])
+        self.assertEqual(out, ["javac", "-d", os.path.join(".please", "test.java_javac"), "-cp", ".", "test.java"])
         
     def test_win_run1(self):
         java = javaconf.JavaWindowsConfigurator()
         out = java.get_run_command("abacaba.java")
-        self.assertEqual(out, ["java", "-cp", ".please", "abacaba"])
+        self.assertEqual(out, ["java", "-cp", os.path.join(".please", "abacaba.java_javac"), "abacaba"])
         
     def test_win_run2(self):
         java = javaconf.JavaWindowsConfigurator()
         out = java.get_run_command("aba/caba.java")
-        self.assertEqual(out, ["java", "-cp", ".please", "caba"])
+        self.assertEqual(out, ["java", "-cp", os.path.join(".please", "caba.java_javac"), "caba"])
         
     def test_linux_compile(self):
         java = javaconf.JavaLinuxConfigurator()
         out = java.get_compile_command("test.java")
-        self.assertEqual(out, ["javac" ,"-d", ".please", "-cp", ".", "test.java"])
+        self.assertEqual(out, ["javac" ,"-d", os.path.join(".please", "test.java_javac"), "-cp", ".", "test.java"])
         
     def test_linux_run(self):
         java = javaconf.JavaLinuxConfigurator()
         out = java.get_run_command("abacaba.java")
-        self.assertEqual(out, ["java" ,"-cp", ".please", "abacaba"])
+        self.assertEqual(out, ["java" ,"-cp", os.path.join(".please", "abacaba.java_javac"), "abacaba"])
         
     def test_linux_run2(self):
         java = javaconf.JavaLinuxConfigurator()
         out = java.get_run_command("aba/caba.java")
-        self.assertEqual(out, ["java", "-cp", ".please", "caba"])
+        self.assertEqual(out, ["java", "-cp", os.path.join(".please", "caba.java_javac"), "caba"])
 
     #TODO: add tests for is_compile_garbage and get_binary_name
     #TODO: add tests for compiling in another dir
