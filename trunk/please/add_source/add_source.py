8482bce846eb252ced492096542b23604934d427
dubov94@gmail.com <dubov94@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-01-05 06:57:27 +0000

    added add and change solution

--- trunk/please/add_source/add_source.py
+++ trunk/please/add_source/add_source.py
@@ -30,41 +30,73 @@ def del_solution(path):
         for num, solve in enumerate(config["solution"]):
             if os.path.abspath(solve["source"]) == abspath:
                 config.delete("solution", num)
-                log.info("Solution has been deleted")
                 writepackage(config.get_text())
+                log.info("Solution has been deleted")
                 return
     raise AddSourceError("There is no such solution")
     
-    
-def add_solution_with_config (package_config, path, expected_list = [], possible_list = []):
+def get_dict_from_args(args):
+    result = {}
+    last_item = None
+    list_items = ["expected_verdicts", "possible_verdicts"]
+    simple_items = ["input", "output"]
+    assigned = True
+    for item in args:
+        if item in simple_items or item in list_items:
+            if assigned == False:
+                raise AddSourceError("Keyword '" + last_item + "' has no assignment")
+            last_item = item
+            assigned = False
+        elif last_item is None:
+            raise AddSourceError("First argument is not a solution keyword")
+        else:
+            if last_item in list_items:
+                result.setdefault(last_item, []).append(item)
+            else:
+                result[last_item] = item
+            assigned = True
+    if assigned == False:
+        raise AddSourceError("Keyword '" + last_item + "' has no assignment")
+    return result
+
+def add_solution (path, args):
     if not os.path.exists(path):
         raise AddSourceError("There is no such file")
     if not os.getcwd() in os.path.abspath(path):
         raise AddSourceError("Solution isn't in problem folder!")
+    
+    package_config = PackageConfig.get_config()
     abspath = os.path.abspath(path)
     if package_config["solution"] is not None:
         for solve in package_config["solution"]:
             if os.path.abspath(solve["source"]) == abspath:
                 raise AddSourceError("There is already such solution")
-    config_file = config.Config("")
-    config_file["source"] = os.path.relpath(path)
-    if expected_list != []:
-        config_file ["expected_verdicts"] = expected_list
-    if possible_list != []:
-        config_file ["possible_verdicts"] = possible_list
-    package_config.set("solution", config_file, None, True)
-
-def add_solution (path, expected_list = [], possible_list = []):
-    package_config = PackageConfig.get_config()
-    add_solution_with_config(package_config, path, expected_list, possible_list)
-    package_text = package_config.get_text()
-    writepackage(package_text)
+            
+    properties = get_dict_from_args(args)
+    new_config = config.Config("")
+    new_config["source"] = os.path.relpath(path)
+    for key, value in properties.items():
+        new_config[key] = value
+        
+    package_config.set("solution", new_config, None, True)
+    writepackage(package_config.get_text())
     log.info("Solution %s has been being added successfully", path)
-    log.debug("Solution %s with expected: %s and possible: %s has been added", path, str(expected_list), str(possible_list))
-
-def change_solution (path, expected_list = [], possible_list = []):
-    del_solution(path)
-    add_solution(path, expected_list, possible_list)
+    
+def change_solution (path, args):
+    config = PackageConfig.get_config()
+    abspath = os.path.abspath(path)
+    if config["solution"] is not None:
+        for num, solve in enumerate(config["solution"]):
+            if os.path.abspath(solve["source"]) == abspath:
+                properties = get_dict_from_args(args)
+                for key, value in properties.items():
+                    solve[key] = value
+                log.info("Solution has been changed")
+                writepackage(config.get_text())
+                return
+    raise AddSourceError("There is no such solution")
+    
+    
 
 def add_checker_with_config (package_config, path):
     if not os.path.exists(path):
