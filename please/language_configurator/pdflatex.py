124eb4a0de8f40442efaa34773dd638245f156c0
gurovic@gmail.com <gurovic@users.noreply.github.com>
2012-02-09 13:53:45 +0000

    tex with pictures inder win and linux

--- please/language_configurator/pdflatex.py
+++ please/language_configurator/pdflatex.py
@@ -1,9 +1,9 @@
 import os
 import os.path
 
-from please.globalconfig import temp_statements_dir
-from please.template.template_utils import get_template_full_path
-
+from ..globalconfig import temp_statements_dir
+from ..template.template_utils import get_template_full_path
+from .language_configurator_utils import is_windows
 
 def is_compilation_garbage(source):
     extensions = [".aux"]
@@ -14,19 +14,28 @@ def is_compilation_garbage(source):
 
 class PDFLatexConfigurator:
     def get_run_command(self, source):
-        bat_name = os.path.join("..", temp_statements_dir, 'maketex.bat')
+        bat_name = os.path.join("..", temp_statements_dir, 'maketex')
+        if is_windows():
+            bat_name = bat_name + '.bat'
+        remark = 'REM ' if is_windows() else '#'
         if not os.path.isfile(bat_name):
             batfile = open(bat_name, "w")
-            batfile.write("REM ===USING png/jpg/jpeg/pdf PICTURES IN TEX===\n\n")
+            if not is_windows():
+                batfile.write('#!/bin/sh\nexport TEXINPUTS="' + get_template_full_path('') + ':.:"\n')
+            batfile.write(remark + "===USING png/jpg/jpeg/pdf PICTURES IN TEX===\n\n")
             for _ in range(2): #always run latex two times to get page numbers etc
-                batfile.write("pdflatex -include-directory=" + get_template_full_path('') + " -output-format=pdf -interaction=nonstopmode -halt-on-error " + source + "\n")
-            batfile.write("\n\nREM ===USING eps PICTURES OR NO PICTURES IN TEX===\n\n")
+                batfile.write("pdflatex -output-format=pdf -interaction=nonstopmode -halt-on-error " + source + "\n")
+            batfile.write("\n\n" + remark + "===USING eps PICTURES OR NO PICTURES IN TEX===\n\n")
             for _ in range(2): #always run latex two times to get page numbers etc
-                batfile.write("REM latex -include-directory=" + get_template_full_path('') + " " + source + "\n")
-            batfile.write("REM dvips " + os.path.splitext(source)[0] + ".dvi" + "\n")
-            batfile.write("REM ps2pdf " + os.path.splitext(source)[0] + ".ps" + "\n")
+                batfile.write(remark + "latex -include-directory=" + get_template_full_path('') + " " + source + "\n")
+            batfile.write(remark + "dvips " + os.path.splitext(source)[0] + ".dvi" + "\n")
+            batfile.write(remark +  "ps2pdf " + os.path.splitext(source)[0] + ".ps" + "\n")
             batfile.close()
-        return ["maketex.bat", ]        
+            os.chmod(bat_name, 755)
+        if is_windows():
+            return ["maketex.bat", ]
+        else:
+            return ["./maketex", ]
 
     def is_compile_garbage (self, source):
         #return is_compilation_garbage(source)
--- please/language_configurator/pslatex.py
+++ please/language_configurator/pslatex.py
@@ -1,8 +1,9 @@
 import os
 import os.path
-from please.globalconfig import temp_statements_dir
 
+from please.globalconfig import temp_statements_dir
 from please.template.template_utils import get_template_full_path
+from .language_configurator_utils import is_windows
 
 def is_compilation_garbage(source):
     extensions = [".aux"]
@@ -13,19 +14,29 @@ def is_compilation_garbage(source):
 
 class PSLatexConfigurator:
     def get_run_command(self, source):
-        bat_name = os.path.join("..", temp_statements_dir, 'maketex.bat')
+        bat_name = os.path.join("..", temp_statements_dir, 'maketex')
+        if is_windows():
+            bat_name = bat_name + '.bat'
+        remark = 'REM' if is_windows() else '#'
         if not os.path.isfile(bat_name):
             batfile = open(bat_name, "w")
-            batfile.write("REM ===USING png/jpg/jpeg/pdf PICTURES IN TEX===\n\n")
+            if not is_windows():
+                batfile.write('#!/bin/sh\nexport TEXINPUTS="' + get_template_full_path('') + ':.:"\n')
+            batfile.write(remark + "===USING png/jpg/jpeg/pdf PICTURES IN TEX===\n\n")
             for _ in range(2): #always run latex two times to get page numbers etc
-                batfile.write("REM pdflatex -include-directory=" + get_template_full_path('') + " -output-format=pdf -interaction=nonstopmode -halt-on-error " + source + "\n")
-            batfile.write("\n\nREM ===USING eps PICTURES OR NO PICTURES IN TEX===\n\n")
+                batfile.write(remark + "pdflatex -output-format=pdf -interaction=nonstopmode -halt-on-error " + source + "\n")
+            batfile.write("\n\n" + remark + "===USING eps PICTURES OR NO PICTURES IN TEX===\n\n")
             for _ in range(2): #always run latex two times to get page numbers etc
                 batfile.write("latex -include-directory=" + get_template_full_path('') + " " + source + "\n")
             batfile.write("dvips " + os.path.splitext(source)[0] + ".dvi" + "\n")
             batfile.write("ps2pdf " + os.path.splitext(source)[0] + ".ps" + "\n")
             batfile.close()
-        return ["maketex.bat", ]        
+            os.chmod(bat_name, 755)
+        if is_windows():
+            return ["maketex.bat", ]
+        else:
+            return ["./maketex", ]
+
 
     def is_compile_garbage (self, source):
         #return is_compilation_garbage(source)
