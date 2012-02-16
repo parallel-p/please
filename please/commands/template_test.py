f41f58ebd6464456d04617b4d87c4688f83aaccb
alex0player@gmail.com <alex0player@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-02-16 16:23:37 +0000

    Keeping sophisticating commands package. Now Matcher takes into account not only first match, but tries to determine the best; it also prints warning if more that one matches is detected. Template constructor (and Matcher.add) now support help parameter; help for Template, with prepended well-formatted template string repersenation, can be accessed as its .help attribute.

--- please/commands/template_test.py
+++ please/commands/template_test.py
@@ -6,31 +6,53 @@ class TemplateTest(unittest.TestCase):
     def _test(self, template, args, **dict):
         t = Template(template)
         args = args.split()
-        self.assertEqual(t.match(args, 'tmp'), dict)
+        self.assertEqual(t.match(args), dict)
 
     def test_correct(self):
-
+        # Simple cases
         self._test("add tag $tag", "add tag a", tag = "a")
         self._test("add tag tag...", "add tag a b c", tag = "a b c".split())
         self._test("[add] tag $tag", "add tag a", tag = "a")
         self._test("[ add ] tag $tag", "tag a", tag = "a")
 
+        # Or-clause.
         self._test("svn praise|blame /path",
                    "svn praise a.py",
-                   path = _j("tmp", "a.py"))
+                   path = "a.py")
 
         self._test("svn praise|blame /path",
                    "svn blame a.py",
-                   path = _j("tmp", "a.py"))
+                   path = "a.py")
 
         self._test("add tag[s] tags... to file /project",
                    "add tags a b c to file helloworld.py",
                    tags = "a b c".split(),
-                   project = _j("tmp", "helloworld.py"))
+                   project = "helloworld.py")
+
+        # Arg - list
         self._test("generate with /file tests...",
                    "generate with generator.py test1 test2 test3",
-                   file = _j("tmp", "generator.py"),
+                   file = "generator.py",
                    tests = "test1 test2 test3".split())
+
+        # List - arg
+        self._test("merge-into files... /file",
+                   "merge-into variant.1 variant.2 variant.3 everything",
+                   files = 'variant.1 variant.2 variant.3'.split(),
+                   file = 'everything')
+
+        # Mistakes
+        self._test("please make me sandwich", "please make me sandwitch")
+        self._test("svn blame /file", "svm blam test.py", file = 'test.py')
+        self._test("ls files...", "ld file1 file2 file3",
+                   files = 'file1 file2 file3'.split())
+
+    def _test_unmatch(self, template, args):
+        self.assertEqual(Template(template).match(args.split()), None)
+
+    def test_incorrect(self):
+        self._test_unmatch("and now for", "something completely different")
+        self._test_unmatch("add [tag] $tag", "tag tag1")
 
 if __name__ == "__main__":
     unittest.main()
