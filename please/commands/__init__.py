efd71e78587633b7e431179c5f39e2fe02ce8bab
alex0player@gmail.com <alex0player@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-02-17 20:08:15 +0000

    Added basic machinery for initialization.

--- please/commands/__init__.py
+++ please/commands/__init__.py
@@ -1,1 +1,15 @@
 from .matcher import Matcher
+
+_matchers = {}
+def get_matcher(name):
+    if name not in _matchers:
+        _matchers[name] = Matcher() # some day will be more sophisticated
+    return matcher[name]
+
+def init_please_matcher():
+    m = get_matcher('please')
+    from . import config
+    for mod in config.modules:
+        m.add_module(mod)
+    return m
+
