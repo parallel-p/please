263c1ed3e5cf696fda1772e81bed8bb3a8349503
dubov94@gmail.com <dubov94@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-04-06 21:11:29 +0000

    fixed issue 120

--- trunk/please/utils/exceptions.py
+++ trunk/please/utils/exceptions.py
@@ -1,5 +1,11 @@
-class PleaseException(Exception):
-    pass
+import traceback
+from .. import globalconfig
 
+class PleaseException(Exception):
+    def __init__(self, *args):
+        super(PleaseException, self).__init__(args)
+        if globalconfig.DEBUG_MODE:
+            traceback.print_stack()        
+        
 class MatcherException(Exception):
-    pass 
+    pass
