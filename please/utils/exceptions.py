43ea6f11aef505c7d375f06767080a64c625f1d5
dubov94@gmail.com <dubov94@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-04-07 22:00:54 +0000

    fixed issue 220

--- please/utils/exceptions.py
+++ please/utils/exceptions.py
@@ -2,8 +2,8 @@ import traceback
 from .. import globalconfig
 
 class PleaseException(Exception):
-    def __init__(self, *args):
-        super(PleaseException, self).__init__(args)
+    def __init__(self, msg):
+        super(PleaseException, self).__init__(msg)
         if globalconfig.DEBUG_MODE:
             traceback.print_stack()        
         
