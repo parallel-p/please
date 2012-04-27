7acd27fb63eaf379af985aaf18a0be219a0d3df8
alex0player@gmail.com <alex0player@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-04-27 10:19:55 +0000

    Replaced re with fnmatch in generator masks; closes issue 207.

--- please/diff_test_finder/diff_test_finder_test.py
+++ please/diff_test_finder/diff_test_finder_test.py
@@ -11,23 +11,23 @@ class TestDiffTestFinder(unittest.TestCase):
         pass
 
     def test_full_01(self):
-        tmp = DiffTestFinder('.*', '')
+        tmp = DiffTestFinder('*', '')
         tests, trash = tmp.tests('.', [[], []], 'stdout')
         self.assertEqual(tests[0].str, 'stdout')
         self.assertEqual(tests[1:], [])
 
     def test_full_02(self):
-        tmp = DiffTestFinder('.*', '')
+        tmp = DiffTestFinder('*', '')
         self.assertEqual(tmp.tests('.', [[], []])[0], [])
 
     def test_full_03(self):
-        tmp = DiffTestFinder(os.path.join('\\.\\.' , '.*'), '.*trash$')
+        tmp = DiffTestFinder(os.path.join('..' , '*'), '*trash')
         tests = tmp.tests('gen', [[], ['01.in', 'trash', 'trash01', os.path.join('gen', '01.in')]])[0]
         correct = ['01.in', 'trash01']
         self.assertSetEqual(set(tests), set(correct))
 
     def test_full_04(self):
-        tmp = DiffTestFinder(os.path.join('\\.\\.', '\w*$'), '.*trash$')
+        tmp = DiffTestFinder(os.path.join('..', '*'), '*trash*')
         tests = tmp.tests('gen', [[], [os.path.join('gen', '01.in'), 'trash',
                                        os.path.join('..', 'trash01'),
                                        os.path.join('gen', '01.in')]])[0]
@@ -35,7 +35,7 @@ class TestDiffTestFinder(unittest.TestCase):
         self.assertSetEqual(set(tests), set(correct))
 
     def test_full_05(self):
-        tmp = DiffTestFinder(os.path.join('\\.\\.', '\w*$'), '.*trash$')
+        tmp = DiffTestFinder(os.path.join('..', '*'), '*trash*')
         tests = tmp.tests('gen', [[], [os.path.join('gen', '01.in'), 'trash',
                                        os.path.join('..', 'trash01'),
                                        os.path.join('gen', '01.in')]], 'stdout.out')[0]
