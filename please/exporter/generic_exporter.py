99c2d9e5f087e4efabad5d81d944c3d813b79097
a.max95@gmail.com <a.max95@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-07-30 06:04:03 +0000

    fixed ejudge exporter

--- please/exporter/generic_exporter.py
+++ please/exporter/generic_exporter.py
@@ -18,6 +18,8 @@ class GenericExporter:
             #    conf = Config(f.read())
             if self.archiver:
                 self.archiver.add(conf['checker'], os.path.join(problem, os.path.split(conf['checker'])[-1]))
+            for testlib in ["testlib.h", "testlib.pas"]:
+                self.archiver.add(os.path.join(problem, testlib))
             #if not os.path.exists(conf['checker']):
             #    print(conf['checker'])
             #    print(-1/0)
