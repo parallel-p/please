9f232cb1aea6b0f26b11e76f352aad28cbf9f166
gurovic@gmail.com <gurovic@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-08-25 08:53:13 +0000

    lang determine test fixed

--- please/lang_config/detection_test_files/helloworld.cs
+++
@@ -1,9 +0,0 @@
-//Hello World in c#
-//But will be determined as c++, 'cause lang_config does not know c#
-class HelloWorld
-{
-    static void Main()
-    {
-        System.Console.WriteLine("Hello, World!");
-    }
-}
