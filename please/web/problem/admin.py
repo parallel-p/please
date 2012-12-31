f27d233fd8ed93f9a779c1dc973b8b378baa8131
dima_tomp@inbox.ru <dima_tomp@inbox.ru@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-12-31 06:12:40 +0000

    Corrected admin.py (web)

--- please/web/problem/admin.py
+++ please/web/problem/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from problem.models import *
+from problem.models import RunErrorDescription, TestGeneratorError,
+                           TestError, TestGeneratorTag, TestGenerator, Test,
+                           Tag, Solution, Verdict
 
-
-for model in (RunErrorDescription, TestGeneratorError, TestError, TestGeneratorTag, TestGenerator, Test, Tag, Solution, Verdict):
-	admin.site.register(model)
-
+admin.site.register(RunErrorDescription)
+admin.site.register(TestGeneratorError)
+admin.site.register(TestError)
+admin.site.register(TestGeneratorTag)
+admin.site.register(TestGenerator)
+admin.site.register(Test)
+admin.site.register(Tag)
+admin.site.register(Solution)
+admin.site.register(Verdict)
