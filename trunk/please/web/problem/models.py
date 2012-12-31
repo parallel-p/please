3122a429ca5f80b47d61a458776956d8d55fed54
dima_tomp@inbox.ru <dima_tomp@inbox.ru@cb711904-5dbb-0428-5097-0481b5fa17eb>
2012-12-31 05:25:03 +0000

    Added Tag, Solution and Verdict models (web)

--- trunk/please/web/problem/models.py
+++ trunk/please/web/problem/models.py
@@ -1,2 +1,25 @@
 from django.db import models
 
+class Problem(models.Model):
+	pass
+
+class Tag(models.Model):
+	name = models.CharField(max_length=64)
+
+	def __str__(self):
+		return str(self.name)
+
+class Solution(models.Model):
+	tag = models.ManyToManyField(Tag)
+	problem = models.ForeignKey(Problem)
+	fname = models.CharField(max_length=256)
+	path_or_stdin = models.BooleanField()
+	path_or_stdout = models.BooleanField()
+
+class Verdict(models.Model):
+	solution = models.ForeignKey(Solution)
+	name = models.CharField(max_length=2)
+	expected = models.BooleanField()
+
+	def __str__(self):
+		return str(self.solution) + ': ' + str(self.name) + (' (possible)' if not bool(self.expected) else ' (expected)')
