e5fd04eaa8057077442acff5cf82005190a218c6
ComradePetr@gmail.com <ComradePetr@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-01 19:04:06 +0000

    Some fixes in urls.py

--- please/web/web/urls.py
+++ please/web/web/urls.py
@@ -6,4 +6,6 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^problems/', 'problem.views.problems_search'),
+    url(r'^problem/(?P<id>\d+)/materials/edit/$', 'problem.views.edit_problem_materials'),
+    url(r'^problem/confirmation', 'problem.views.problem_edit_confirm'),
 )
