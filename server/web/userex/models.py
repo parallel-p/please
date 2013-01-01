42fc0e0615756ebc124b8aef862d32eef15dadb6
riazanovskiy@gmail.com <riazanovskiy@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-01 12:06:51 +0000

    Optimized user creation

--- server/web/userex/models.py
+++ server/web/userex/models.py
@@ -1,11 +1,12 @@
 from django.db.models import signals
 from django.dispatch import receiver
-import linux_user
 from django.contrib.auth.models import User
 
+import linux_user
 
-@receiver(signals.post_init)
+
+@receiver(signals.post_save)
 def create_user_callback(sender, **kwargs):
     if sender == User:
-        if 'instance' in kwargs:
+        if 'created' in kwargs and kwargs['created'] and 'instance' in kwargs:
             linux_user.register_user(kwargs['instance'].username)
