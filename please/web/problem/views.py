c68178b615b594bfcb8a7fdb85e0b0aae6a4c3d1
dima_tomp@inbox.ru <dima_tomp@inbox.ru@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-01 13:09:20 +0000

    Added path field to Problem (web)

--- please/web/problem/views.py
+++ please/web/problem/views.py
@@ -1,1 +1,21 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from django.http import redirect
+from problem.views import ProblemEditForm
+from problem.models import Problem
+
+def edit_problem_materials(request, id=None):
+	if id is None:
+		if request.method == 'POST':
+			pass
+		else:
+			form = ProblemEditForm()
+	else:
+		if request.mathod == 'POST':
+			pass
+		else:
+			form = ProblemEditForm(instance=Problem.objects.get(id=id))
+			form.load_files()
+	return render_to_response(request, 'edit_problem_materials.html', {
+			'form': Form,
+		})
