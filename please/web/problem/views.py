80253fdf1efb8b86dccf4c02eef61691fe7b9a37
denshion@mail.ru <denshion@mail.ru@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-01 14:48:57 +0000

    Added list_problems view and template

--- please/web/problem/views.py
+++ please/web/problem/views.py
@@ -1,18 +1,23 @@
 # Create your views here.
 from django.shortcuts import render_to_response
-from django.http import redirect
-from problem.forms import ProblemEditMaterialsForm
+#from django.http import redirect
+#from problem.forms import ProblemEditMaterialsForm
 from problem.models import Problem
+from django.template import RequestContext
 
-def edit_problem_materials(request, id=None):
-	if request.method == 'POST':
-		form = ProblemEditForm(request.POST, instance=Problem.objects.get(id=id))
-		if form.is_valid():
+#def edit_problem_materials(request, id=None):
+#	if request.method == 'POST':
+#		form = ProblemEditForm(request.POST, instance=Problem.objects.get(id=id))
+#		if form.is_valid():
+#
+#			return redirect('/thanks/')
+#	else:
+#		form = ProblemEditMaterialsForm(instance=Problem.objects.get(id=id))
+#		form.load_files()
+#	return render_to_response(request, 'edit_problem_materials.html', {
+#			'form': Form,
+#		})# Create your views here.
 
-			return redirect('/thanks/')
-	else:
-		form = ProblemEditMaterialsForm(instance=Problem.objects.get(id=id))
-		form.load_files()
-	return render_to_response(request, 'edit_problem_materials.html', {
-			'form': Form,
-		})
+def list_problems(request, problem_list=Problem.objects.all()):
+    return render_to_response('list_problems.html', {'problems' : problem_list},
+        context_instance=RequestContext(request))
