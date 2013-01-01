279d52b53cc1c2f9f3e02f5f34ef348606cd4ab4
ComradePetr@gmail.com <ComradePetr@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-01 12:46:02 +0000

    Added clone repository and tests for it

--- server/clone_repository_test.py
+++ server/clone_repository_test.py
@@ -3,12 +3,12 @@ import subprocess
 import stat
 import shutil
 import unittest
-from create_repository import *
+from clone_repository import *
 
-dirn = ["myfold", "myfold_repo"]
+dirn = ["myfold_repo1", "myfold_repo2"]
 
 
-class CreateRepositoryFromDir_test(unittest.TestCase):
+class CloneRepository_test(unittest.TestCase):
     def tearDown(self):
         for dirname0 in dirn:
             for dirpath, dirnames, filenames \
@@ -19,7 +19,7 @@ class CreateRepositoryFromDir_test(unittest.TestCase):
                     os.remove(filename)
                 os.rmdir(dirpath)
 
-    def test_create(self):
+    def test_clone(self):
         def addFiles():
             os.mkdir(dirn[0])
             f = open(os.path.join(dirn[0], ".gitignore"), "w")
@@ -30,17 +30,32 @@ class CreateRepositoryFromDir_test(unittest.TestCase):
             f.write("release")
             f.close()
 
+        def gitCommand(folder, command_line):  # "folder" is subdirectory
+            os.chdir(folder)
+            try:
+                out = subprocess.check_output(command_line)
+            finally:
+                os.chdir("..")
+            return out
+
         addFiles()
-        create_repository_from_dir(dirn[0], dirn[1],
-                                   User("Username", "mail"))
-        os.chdir(dirn[1])
+        gitCommand(dirn[0], "git init")
+        gitCommand(dirn[0], "git add .")
+        gitCommand(dirn[0], "git commit -m 'Init'")
+        clone_repository(dirn[0], dirn[1])
+
+        out = [""] * 2
         command_line = "git ls-tree -r --name-only --full-tree master"
-        try:
-            out = subprocess.check_output(command_line)
-        finally:
-            os.chdir("..")
-        self.assertEqual(set(out.decode("ASCII").split("\n")[:-1]),
-                         set([".gitignore", "sub/abacaba"]))
+        for i, dirname in enumerate(dirn):
+            out[i] = gitCommand(dirn[1], command_line)
+        self.assertEqual(out[0], out[1])
+
+    def test_dir_exists(self):
+        for dirname in dirn:
+            os.mkdir(dirname)
+        with self.assertRaises(OSError):
+            clone_repository(dirn[0], dirn[1])
+
 
 if __name__ == '__main__':
     unittest.main()
