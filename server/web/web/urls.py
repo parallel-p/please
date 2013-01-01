bcf9437b69692c9303b235a0661990121e26bab8
riazanovskiy@gmail.com <riazanovskiy@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-01 14:43:48 +0000

    Added logout button

--- server/web/web/urls.py
+++ server/web/web/urls.py
@@ -7,8 +7,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html',
-                                                         'redirect_field_name': '/home'}),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'^home/$', 'userex.views.home'),
     url(r'^$', 'userex.views.index'),
 )
