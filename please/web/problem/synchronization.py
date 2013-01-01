f28cd62ddd55f91ede58505d9387c366cbec9f63
Dima239Philippov@gmail.com <Dima239Philippov@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-01 12:20:13 +0000

    Modified synchronization

--- please/web/problem/synchronization.py
+++ please/web/problem/synchronization.py
@@ -1,10 +1,37 @@
-from ...package.package_config import get_config
-from ... import globalconfig
-from ...add_source.add_source import add_solution
+from please.package.package_config import PackageConfig
+from please import globalconfig
+from please.add_source.add_source import add_solution
 
+
+def import_to_database(model, path=None, name=globalconfig.default_package):
+    conf = PackageConfig.get_config(path, name)
+
+    model.name = conf["name"]
+    model.short_name = conf["shortname"]
+    model.type = ''
+    
+    model.input = conf["input"]
+    model.output = conf["output"]
+    model.time_limit = conf["time_limit"]
+    model.memory_limit = conf["memory_limit"]
+
+    model.checker = conf["checker"]
+    model.validator = conf["validator"]
+    model.main_solution = conf["main_solution"]
+
+    model.statement = conf["statement"]
+    model.description = conf["description"]
+
+    model.hand_answer_extension = conf["hand_answer_extension"]
+
+    #TODO: well_done tests and answers should be ManyToMany, not strings
+    model.well_done_test = ", ".join(conf["well_done_test"])
+    model.well_done_answer = ", ".join(conf["well_done_answer"])
+                                   
+    model.analysis = conf["analysis"]
 
 def import_from_database(model, path=None, name=globalconfig.default_package):
-	conf = get_config(path, name)
+	conf = PackageConfig.get_config(path, name)
 	conf.name = str(model.name)
 	conf.shortname = str(model.short_name)
 	conf.set('tags', list(map(str, model.tags.all())))
@@ -26,9 +53,9 @@ def import_from_database(model, path=None, name=globalconfig.default_package):
 	for solution in model.solution_set.all():
 		args = []
 		if solution.path_or_stdin:
-			args += ['input', str(solution.path_or_stdin)]
+			args += ['input', str(solution.input)]
 		if solution.path_or_stdout:
-			args += ['output', str(solution.path_or_stdout)]
+			args += ['output', str(solution.output)]
 		if solution.possible_verdicts.count() != 0:
 			args += (['possible'] +
 			        list(map(str, solution.possible_verdicts.all())))
