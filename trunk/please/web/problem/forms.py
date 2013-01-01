acc303e4ac07de3265377beecf6af3348f9092d3
ComradePetr@gmail.com <ComradePetr@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-01 19:01:43 +0000

    Problems search by tag

--- trunk/please/web/problem/forms.py
+++ trunk/please/web/problem/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 
+
+class ProblemSearch(forms.Form):
+    tags = forms.CharField(required=False)
+
+
 class ProblemEditMaterialsForm(forms.Form):
-	statement = forms.CharField(widget=forms.Textarea, required=False)
-	description = forms.CharField(widget=forms.Textarea, required=False)
-	analysis = forms.CharField(widget=forms.Textarea, required=False)
+    statement = forms.CharField(widget=forms.Textarea, required=False)
+    description = forms.CharField(widget=forms.Textarea, required=False)
+    analysis = forms.CharField(widget=forms.Textarea, required=False)
