9db585e3735c5bf7b4235d2971c6828c4d7f7ef5
dima_tomp@inbox.ru <dima_tomp@inbox.ru@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-01 13:37:56 +0000

    Added formspy

--- trunk/please/web/problem/views.py
+++ trunk/please/web/problem/views.py
@@ -1,21 +1,18 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.http import redirect
-from problem.views import ProblemEditForm
+from problem.forms import ProblemEditMaterialsForm
 from problem.models import Problem
 
 def edit_problem_materials(request, id=None):
-	if id is None:
-		if request.method == 'POST':
-			pass
-		else:
-			form = ProblemEditForm()
+	if request.method == 'POST':
+		form = ProblemEditForm(request.POST, instance=Problem.objects.get(id=id))
+		if form.is_valid():
+
+			return redirect('/thanks/')
 	else:
-		if request.mathod == 'POST':
-			pass
-		else:
-			form = ProblemEditForm(instance=Problem.objects.get(id=id))
-			form.load_files()
+		form = ProblemEditMaterialsForm(instance=Problem.objects.get(id=id))
+		form.load_files()
 	return render_to_response(request, 'edit_problem_materials.html', {
 			'form': Form,
 		})
