70b9f9b1e66c36773da28af1754b6aeef124e1d8
arthur@khashaev.ru <arthur@khashaev.ru@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-02 09:49:17 +0000

    added problem_sync helper

---
+++ trunk/please/web/problem/helpers.py
@@ -0,0 +1,20 @@
+from problem.models import Problem
+from problem.synchronization import import_to_database, export_from_database
+
+def problem_sync(read=True, write=False):
+    def wrapped_decorator(function):
+        def wrapped_function(request, id, *args, **kwargs):
+            if read:
+                try:
+                    problem = Problem.objects.get(id=id)
+                except Problem.DoesNotExist:
+                    problem = None
+                if problem:
+                    import_to_database(problem)
+                    problem.save()
+            response = function(request, id, *args, **kwargs)
+            if write:
+                export_from_database(Problem.objects.get(id=id))
+            return response
+        return wrapped_function
+    return wrapped_decorator
