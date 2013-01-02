5b0529ec79d8b4877f180f622aa2983b34ed4fa1
riazanovskiy@gmail.com <riazanovskiy@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-02 09:29:11 +0000

    Urls changed

--- please/web/problem/tests.py
+++ please/web/problem/tests.py
@@ -1,8 +1,10 @@
 import django.test
 from .models import Problem, ProblemTag
 
+
 def names(problems):
     return [x.name for x in problems]
+
 
 def add_problem(problem, tag):
     p = Problem(name=problem)
@@ -13,8 +15,8 @@ def add_problem(problem, tag):
     p.save()
     t.save()
 
-class ViewTest(django.test.TestCase):
 
+class ViewTest(django.test.TestCase):
     def setUp(self):
         add_problem("first", "gcd")
         add_problem("second", "lcm")
@@ -27,15 +29,14 @@ class ViewTest(django.test.TestCase):
         self.assertTemplateUsed(resp, "problems_list.html")
 
     def test_problems_by_tags(self):
-        resp = self.client.get("/problems/", {"tags" : "lcm"})
+        resp = self.client.get("/problems/", {"tags": "lcm"})
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(names(resp.context["problems"]),
                 ["second"])
         self.assertTemplateUsed(resp, "problems_list.html")
 
-        resp = self.client.get("/problems/", {"tags" : "gcd"})
+        resp = self.client.get("/problems/", {"tags": "gcd"})
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(names(resp.context["problems"]),
                 ["first"])
         self.assertTemplateUsed(resp, "problems_list.html")
-
