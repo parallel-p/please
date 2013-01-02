76ba5a498677e157dc5ebf3b37ef54952a17e613
dima_tomp@inbox.ru <dima_tomp@inbox.ru@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-02 08:56:39 +0000

    Returned main_solution field back (web)

--- trunk/please/web/problem/views.py
+++ trunk/please/web/problem/views.py
@@ -22,20 +22,32 @@ def todo(request, id):
 
 def add_problem_files(request, id):
     model = Problem.objects.get(id=id)
-    checker_abspath = os.path.join(str(model.path), str(model.checker_path))
-    validator_abspath = os.path.join(str(model.path), str(model.validator_path))
     if request.method == 'POST':
         form = ProblemUploadFilesForm(request.POST, request.FILES)
         if form.is_valid() and len(request.FILES) > 0:
             if 'checker' in request.FILES.keys():
-                open(checker_abspath, 'wb').write(request.FILES['checker'].read())
+                checker_fpath, checker_fname = os.path.split(str(model.checker_path))
+                checker_fname = request.FILES['checker'].name
+                model.checker_path = os.path.join(checker_fpath, checker_fname)
+                open(os.path.join(
+                        str(model.path),
+                        str(model.checker_path)
+                    ), 'wb').write(request.FILES['checker'].read())
             if 'validator' in request.FILES.keys():
-                open(validator_abspath, 'wb').write(request.FILES['validator'].read())
+                validator_fpath, validator_fname = os.path.split(str(model.validator_path))
+                validator_fname = request.FILES['validator'].name
+                model.validator_path = os.path.join(validator_fpath, validator_fname)
+                open(os.path.join(
+                        str(model.path),
+                        str(model.validator_path)
+                    ), 'wb').write(request.FILES['validator'].read())
+            model.save()
             return redirect('/problem/confirmation/')
     else:
         form = ProblemUploadFilesForm()
     return render_to_response('add_problem_files.html', {
             'form': form,
+            'model': model,
             'id': id
         }, RequestContext(request))
 
