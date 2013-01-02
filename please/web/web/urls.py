cfb191bb49dd6c1480cbc987e02db36230f27082
riazanovskiy@gmail.com <riazanovskiy@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-02 09:49:02 +0000

    Typo in urls

--- please/web/web/urls.py
+++ please/web/web/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     url(r'^problem/(?P<id>\d+)/todo/$', 'problem.views.todo'),
     url(r'^problem/(?P<id>\d+)/materials/edit/$', 'problem.views.edit_problem_materials'),
     url(r'^problem/(?P<id>\d+)/files/upload/main$', 'problem.views.add_problem_files', name='file_upload'),
-    url(r'^problem/(?P<id>\d+)/files/upload/additional$', 'problem.views.add_user_files()'),
+    url(r'^problem/(?P<id>\d+)/files/upload/additional$', 'problem.views.upload_additional'),
     url(r'^problem/(?P<id>\d+)/solutions/add/$', 'problem.views.add_solution'),
     url(r'^problem/confirmation$', TemplateView.as_view(template_name='problem_edit_success.html')),
     url(r'^problem/create', 'problem.views.create_problem')
