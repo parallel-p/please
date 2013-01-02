46d143026e09b79eb66411d4b0489d15cc66053f
arthur@khashaev.ru <arthur@khashaev.ru@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-02 17:57:02 +0000

    added missing $ in urls.py

--- trunk/please/web/web/urls.py
+++ trunk/please/web/web/urls.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
+
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
@@ -11,7 +12,7 @@ urlpatterns = patterns('',
     url(r'^problems/(?P<id>\d+)/files/upload/main$', 'problem.views.files.upload_main', name='file_upload'),
     url(r'^problems/(?P<id>\d+)/files/upload/additional$', 'problem.views.files.upload_additional'),
     url(r'^problems/(?P<id>\d+)/solutions/add/$', 'problem.views.solutions.add', name='solution-add'),
-    url(r'^problems/(?P<id>\d+)/tests/', 'problem.views.tests.show'),
+    url(r'^problems/(?P<id>\d+)/tests/$', 'problem.views.tests.show'),
     url(r'^problems/confirmation/$', TemplateView.as_view(template_name='problem_edit_success.html')),
     url(r'^problems/create/$', 'problem.views.problems.create'),
     url(r'^problems/$', 'problem.views.problems.search_by_tag', name="problem-list"),
