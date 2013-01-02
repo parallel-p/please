aa649c9872c35b216a67e3d96c044d84ef860154
dima_tomp@inbox.ru <dima_tomp@inbox.ru@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-02 05:26:38 +0000

    Replaced problem/confirmation function with TemplateView (web)

--- trunk/please/web/web/urls.py
+++ trunk/please/web/web/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,5 +8,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^problems/', 'problem.views.problems_search'),
     url(r'^problem/(?P<id>\d+)/materials/edit/$', 'problem.views.edit_problem_materials'),
-    url(r'^problem/confirmation', 'problem.views.problem_edit_confirm'),
+    url(r'^problem/confirmation', TemplateView.as_view(template_name='problem_edit_success.html')),
 )
