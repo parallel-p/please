631cae508289ded79c38d7d72c1b92e6f9c79702
Dima239Philippov@gmail.com <Dima239Philippov@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-02 09:32:19 +0000

    ProblemEditForm added

--- please/web/problem/forms.py
+++ please/web/problem/forms.py
@@ -2,6 +2,12 @@ from django import forms
 from problem.models import *
 
 
+class ProblemEditForm(forms.ModelForm):
+    class Meta:
+        model = Problem
+        fields = ('path', 'name', 'input', 'output', 'time_limit', 'memory_limit')
+
+
 class ProblemSearch(forms.Form):
     tags = forms.CharField(required=False)
 
