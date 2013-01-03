4b277971e8e6e56b9753a0c2e125b3f636b17593
arthur@khashaev.ru <arthur@khashaev.ru@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-03 08:53:59 +0000

    todo splitted to view and block

--- please/web/problem/views/todo.py
+++ please/web/problem/views/todo.py
@@ -4,11 +4,14 @@ from please.todo.todo_generator import TodoGenerator
 from problem.models import Problem
 
 
-@problem_sync(read=True, write=False)
-def show(request, id):
-    problem = get_object_or_404(Problem, id=id)
-    return render_to_response('todo.html', {
+def show_block(problem):
+    return {
         'status_description': TodoGenerator.get_status_description(problem.path),
         'tests_count': TodoGenerator.generated_tests_count(problem.path),
         'samples_count': TodoGenerator.sample_tests_count(problem.path),
-    })
+    }
+
+@problem_sync(read=True, write=False)
+def show(request, id):
+    problem = get_object_or_404(Problem, id=id)
+    return render_to_response('todo.html', { 'todo': show_block(problem) })
