498830c9c26b7a44d25c21cde7ba4db438ba51a1
riazanovskiy@gmail.com <riazanovskiy@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-03 09:58:52 +0000

    Minor fixes

--- please/web/problem/fixtures/problems/cubes/solutions/solution.py
+++ please/web/problem/fixtures/problems/cubes/solutions/solution.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-masha=[]
-pasha=[]
-ansmasha=[]
-anspasha=[]
-intersect=[]
+masha = []
+pasha = []
+ansmasha = []
+anspasha = []
+intersect = []
 n, m = list(map(int, input().split()))
 for a in range(n):
     masha.append(int(input()))
@@ -11,19 +11,19 @@ for a in range(m):
     pasha.append(int(input()))
 masha.sort()
 pasha.sort()
-i=0
-j=0
+i = 0
+j = 0
 while(i < n and j < m):
     if(masha[i] < pasha[j]):
         ansmasha.append(masha[i])
-        i+=1
+        i += 1
     elif(masha[i] > pasha[j]):
         anspasha.append(pasha[j])
-        j+=1
+        j += 1
     elif(masha[i] == pasha[j]):
         intersect.append(masha[i])
-        i+=1
-        j+=1
+        i += 1
+        j += 1
 for a in range(i, n):
     ansmasha.append(masha[a])
 for a in range(j, m):
