d5f405bac9a4db733c94b9b929deefdc4fbba0e9
arthur@khashaev.ru <arthur@khashaev.ru@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-03 05:50:34 +0000

    added sample initial data

---
+++ please/web/problem/fixtures/problems/cubes/validator.cpp
@@ -0,0 +1,27 @@
+#include "testlib.h"
+
+using namespace std;
+
+int main()
+{
+    registerValidation();
+    int n, m;
+    n = inf.readInt(0, 100000);
+    inf.readChar(' ');
+    m = inf.readInt(0, 100000);
+    inf.readEoln();
+    for(int i = 0; i < n; i++)
+    {
+        inf.readInt(0, 1000000000);
+        inf.readEoln();
+    }
+    for(int i = 0; i < m; i++)
+    {
+        inf.readInt(0, 1000000000);
+        inf.readEoln();
+    }
+    inf.readEof();
+
+    return 0;
+}
+
