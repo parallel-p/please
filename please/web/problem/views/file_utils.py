3ea3507f4faf28e44300795dce52b0e9cc940dd0
ComradePetr@gmail.com <ComradePetr@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-03 08:45:00 +0000

    File read and write

--- please/web/problem/views/file_utils.py
+++ please/web/problem/views/file_utils.py
@@ -7,3 +7,13 @@ def file_save(temp_file, path):
         for chunk in temp_file.chunks():
             destination.write(chunk)
     return name
+
+
+def file_write(text, filename):
+    with open(filename, 'w', newline='', encoding='utf-8') as source:
+        source.write(text)
+
+
+def file_read(filename):
+    with open(filename, 'r', newline='', encoding='utf-8') as destination:
+        return destination.read()
