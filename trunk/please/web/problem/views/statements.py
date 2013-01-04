c8cc3e6f05f5f977b736353ebce9fceecfb4e6bb
arthur@khashaev.ru <arthur@khashaev.ru@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-04 09:54:43 +0000

    brand new design

--- trunk/please/web/problem/views/statements.py
+++ trunk/please/web/problem/views/statements.py
@@ -5,12 +5,12 @@ from problem.models import Problem
 from problem.views import materials, todo
 
 
-@problem_sync(read=True, write=False)
+@problem_sync(read=False, write=False)
 def common(request, id):
     problem = get_object_or_404(Problem, id=id)
     if request.method == 'POST' and 'save_and_generate' in request.POST:
-    	materials.edit_dict(request, id)
-    	return materials.gen_statement(request, id)
+        materials.edit_dict(request, id)
+        return materials.gen_statement(request, id)
     return render_to_response('statements.html', {
         'problem_id': id,
         'edit_dict': materials.edit_dict(request, id),
