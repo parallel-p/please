6f0fb9fffeb91c2d5c1464764affc3b35a669a76
arthur@khashaev.ru <arthur@khashaev.ru@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-04 11:49:42 +0000

    statements error processing

--- trunk/please/web/problem/views/statements.py
+++ trunk/please/web/problem/views/statements.py
@@ -3,16 +3,22 @@ from django.template import RequestContext
 from problem.helpers import problem_sync
 from problem.models import Problem
 from problem.views import materials, todo
+from please.utils.exceptions import PleaseException
 
 
 @problem_sync(read=False, write=False)
 def common(request, id):
     problem = get_object_or_404(Problem, id=id)
+    error = None
     if request.method == 'POST' and 'save_and_generate' in request.POST:
         materials.edit_dict(request, id)
-        return materials.gen_statement(request, id)
+        try:
+            return materials.gen_statement(request, id)
+        except PleaseException as e:
+            error = str(e)
     return render_to_response('statements.html', {
         'problem': problem,
         'edit_dict': materials.edit_dict(request, id),
         'todo': todo.show_block(problem),
+        'error': error,
     }, RequestContext(request))
