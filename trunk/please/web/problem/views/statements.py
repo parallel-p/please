a1abb3c9bfecc7a1a2e4b44b5f741c13327d4011
arthur@khashaev.ru <arthur@khashaev.ru@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-04 08:41:29 +0000

    save & generate tests button in statements tab

--- trunk/please/web/problem/views/statements.py
+++ trunk/please/web/problem/views/statements.py
@@ -8,6 +8,9 @@ from problem.views import materials, todo
 @problem_sync(read=True, write=False)
 def common(request, id):
     problem = get_object_or_404(Problem, id=id)
+    if request.method == 'POST' and 'save_and_generate' in request.POST:
+    	materials.edit_dict(request, id)
+    	return materials.gen_statement(request, id)
     return render_to_response('statements.html', {
         'problem_id': id,
         'edit_dict': materials.edit_dict(request, id),
