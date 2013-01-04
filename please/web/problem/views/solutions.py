c359d10663250298e96c9773e6c32d176e6fee7a
denshion@mail.ru <denshion@mail.ru@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-04 05:39:33 +0000

    extracted solution adding view to a block

--- please/web/problem/views/solutions.py
+++ please/web/problem/views/solutions.py
@@ -5,37 +5,35 @@ from problem.models import Problem, Solution
 from problem.forms import SolutionAddForm
 import os.path
 from problem.helpers import problem_sync
-
-
-def add_save_file(file, directory):
-    if not os.path.isdir(directory):
-        os.makedirs(directory)
-    fullname = os.path.join(directory, file.name)
-    stream = open(fullname, mode='wb')
-    stream.write(file.read())
-    stream.close()
-    return fullname
+from problem.views.file_utils import file_save
 
 
 @problem_sync(read=True, write=False)
-def add(request, id):
+def add_block(request, problem_id):
     if request.method == 'POST':
         form = SolutionAddForm(request.POST, request.FILES)
         if form.is_valid():
-            solution = Solution(problem=Problem.objects.get(id=id))
-            dir = str(solution.problem.path)
-            solution.path = add_save_file(request.FILES['solution_file'], dir)
-            solution.input = add_save_file(request.FILES['input_file'], dir)
-            solution.output = add_save_file(request.FILES['output_file'], dir)
+            solution = Solution(problem=Problem.objects.get(id=problem_id))
+            dir = os.path.join(str(solution.problem.path), 'solutions')
+            solution.path = file_save(request.FILES['solution_file'], dir)
+            solution.input = file_save(request.FILES['input_file'], dir)
+            solution.output = file_save(request.FILES['output_file'], dir)
             solution.save()
-            for choice in filter(lambda t: t[0] in form.cleaned_data['expected_verdicts'], form.fields['expected_verdicts'].choices):
+            for choice in filter(
+                    lambda t: t[0] in form.cleaned_data['expected_verdicts'],
+                    form.fields['expected_verdicts'].choices):
                 solution.expected_verdicts.add(choice.id)
-            for choice in filter(lambda t: t[0] in form.cleaned_data['possible_verdicts'], form.fields['possible_verdicts'].choices):
+            for choice in filter(
+                    lambda t: t[0] in form.cleaned_data['possible_verdicts'],
+                    form.fields['possible_verdicts'].choices):
                 solution.possible_verdicts.add(choice.id)
             solution.save()
-            return redirect(reverse('problem.views.problems.search_by_tag'))
-            #TODO: change to reverse() here
+            form = SolutionAddForm()
     else:
         form = SolutionAddForm()
-    return render_to_response('add_solution.html', {'form': form, 'id': id},
+    return {'problem_solution_add': {'form' : form}, 'problem_id': problem_id}
+
+def add(request, problem_id):
+    return render_to_response('add_solution.html',
+        add_block(request, problem_id),
         context_instance=RequestContext(request))
