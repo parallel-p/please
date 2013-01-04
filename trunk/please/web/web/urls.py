f43aee2f4f5735da16d6597233e1071f98b57d29
arthur@khashaev.ru <arthur@khashaev.ru@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-04 14:52:16 +0000

    problems tree import

--- trunk/please/web/web/urls.py
+++ trunk/please/web/web/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
 
     url(r'^problems/$', 'problem.views.problems.search_by_tag', name="problem-list"),
     url(r'^problems/add/$', 'problem.views.problems.add'),
+    url(r'^problems/add-tree/$', 'problem.views.problems.add_tree'),
     url(r'^problems/create/$', 'problem.views.problems.create'),
     url(r'^problems/(?P<id>\d+)/edit/$', 'problem.views.problems.create'),
     url(r'^problems/confirmation/$', TemplateView.as_view(template_name='problem_edit_success.html')),
