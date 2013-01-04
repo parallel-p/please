57b9957ab53fc815d4fff234fb92296cffbc080e
dima_tomp@inbox.ru <dima_tomp@inbox.ru@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-04 12:27:39 +0000

    Removed sample tests number

--- trunk/please/web/problem/views/todo.py
+++ trunk/please/web/problem/views/todo.py
@@ -8,11 +8,13 @@ def show_block(problem):
     return {
         'status_description': TodoGenerator.get_status_description(problem.path),
         'tests_count': TodoGenerator.generated_tests_count(problem.path),
-        'samples_count': TodoGenerator.sample_tests_count(problem.path),
+        'samples_present': TodoGenerator.sample_tests_count(problem.path) > 0,
     }
 
 
 @problem_sync(read=True, write=False)
 def show(request, id):
     problem = get_object_or_404(Problem, id=id)
+    a = show_block(problem)
+    print(a['samples_present'])
     return render_to_response('todo.html', {'todo': show_block(problem)})
