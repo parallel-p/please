30f07335f01fb019e3e1c5e3520b62b33342aac4
ComradePetr@gmail.com <ComradePetr@gmail.com@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-04 17:32:59 +0000

    Error handling in manage_tests.py

--- trunk/please/web/problem/views/manage_tests.py
+++ trunk/please/web/problem/views/manage_tests.py
@@ -6,11 +6,13 @@ from problem.views.file_utils import *
 from problem.views.upload_files import *
 from please.command_line.generate_tests import generate_tests, generate_tests_with_tags
 from please import globalconfig
+from please.utils.exceptions import PleaseException
 import os.path
 
 
 def manage_tests(request, problem):
     tp_path = os.path.join(problem.path, globalconfig.default_tests_config)
+    error = None
     if request.method == 'POST':
         form = ManageTestsForm(request.POST)
         if form.is_valid():
@@ -19,13 +21,16 @@ def manage_tests(request, problem):
             if "generate_tests" in request.POST:
                 stags = form.cleaned_data["tags_for_generate_tests"]
                 with ChangeDir(problem.path):
-                    if stags != "":
-                        generate_tests_with_tags(stags.split(" "))
-                    else:
-                        generate_tests()
+                    try:
+                        if stags != "":
+                            generate_tests_with_tags(stags.split(" "))
+                        else:
+                            generate_tests()
+                    except PleaseException as e:
+                        error = e
     else:
         form = ManageTestsForm(initial={"tests_please_content": file_read(tp_path)})
 
-    answer = {'form': form, 'problem_id': problem.id}
+    answer = {'form': form, 'problem_id': problem.id, 'error': error}
     answer.update({'upload_files': upload_files(request, problem)})
     return answer
