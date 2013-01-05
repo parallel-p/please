dbf8316d422f0bbdad9831b515af4e83db3b1b96
arthur@khashaev.ru <arthur@khashaev.ru@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-05 13:18:29 +0000

    unique names in ProblemTag and WellDone models

--- trunk/please/web/problem/models.py
+++ trunk/please/web/problem/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class ProblemTag(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=64, unique=True)
 
     def __str__(self):
         return str(self.name)
 
 
 class WellDone(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=64, unique=True)
 
     def __str__(self):
         return str(self.name)
