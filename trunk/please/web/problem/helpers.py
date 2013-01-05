2ed1bb9129c46f3a793ab664614c9522a525b116
VitalyGoldstein <VitalyGoldstein@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-05 12:55:13 +0000

    fix sync and sync with deletion

--- trunk/please/web/problem/helpers.py
+++ trunk/please/web/problem/helpers.py
@@ -1,6 +1,6 @@
 from problem.models import Problem
 from problem import synchronization
-
+from django.shortcuts import render_to_response
 
 def problem_sync(read=True, write=False):
     def wrapped_decorator(function):
@@ -10,12 +10,22 @@ def problem_sync(read=True, write=False):
                     problem = Problem.objects.get(id=id)
                 except Problem.DoesNotExist:
                     problem = None
+
+                problem_was_deleted = False
+                problem_path = problem.path if problem else None
                 if problem:
-                    synchronization.import_to_database(problem)
-                    problem.save()
-            response = function(request, id, *args, **kwargs)
-            if write:
-                synchronization.export_from_database(Problem.objects.get(id=id))
-            return response
+                    problem = synchronization.import_to_database(problem)
+                    if problem:
+                        problem.save()
+                    else:
+                        problem_was_deleted = True
+            if not problem_was_deleted:
+                response = function(request, id, *args, **kwargs)
+                if write:
+                    synchronization.export_from_database(Problem.objects.get(id=id))
+                return response
+            else:
+                return render_to_response('problem/deleted_problem.html',
+                        {"path": problem_path})
         return wrapped_function
     return wrapped_decorator
