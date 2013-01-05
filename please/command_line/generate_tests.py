cba68565511ad8b95acd61301cc038d5522bba3e
VitalyGoldstein <VitalyGoldstein@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-05 06:57:09 +0000

    del strange logic with solution tags

--- please/command_line/generate_tests.py
+++ please/command_line/generate_tests.py
@@ -1,13 +1,7 @@
 from ..tests_answer_generator import generate, generate_all, AdmitAny, AdmitAll
 
 def generate_tests_with_tags(tags):
-    stags = ' '.join(tags)
-    if ',' in tags:
-        tags = [x.strip() for x in stags.split(',')]
-        admit = AdmitAny
-    else:
-        admit = AdmitAll
-    generate(tags, admit=admit)
+    generate(tags, admit=AdmitAll)
 
 def generate_tests():
     generate_all()						
--- please/tests_answer_generator/__init__.py
+++ please/tests_answer_generator/__init__.py
@@ -3,9 +3,6 @@ from .tests_answer_generator import AdmitAny, AdmitAll
 
 _tg = TestsAndAnswersGenerator()
 
-# these loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong names
-# and too many instances scare me
-
 generate = _tg.generate
 generate_all = _tg.generate_all
 validate = _tg.validate
