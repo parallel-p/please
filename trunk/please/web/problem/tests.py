1b56698088233f8ce66f2ca993a101740b35bb4b
dima_tomp@inbox.ru <dima_tomp@inbox.ru@cb711904-5dbb-0428-5097-0481b5fa17eb>
2013-01-06 07:10:55 +0000

    Added sync test

--- trunk/please/web/problem/tests.py
+++ trunk/please/web/problem/tests.py
@@ -2,4 +2,4 @@ from problem.views.problems_test import ProblemsTests
 from problem.views.solutions_test import SolutionsTests
 from problem.views.todo_test import TodoTests
 from problem.views.files_test import AdditionalUploadTest
-# from problem.synchronization_test import ImportExportIntegrationTest
+from problem.synchronization_test import SynchronizationTest
